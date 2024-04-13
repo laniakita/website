31aca5536ea9d22b2d839b16452bac7c43e76ec8
laniakita <101172492+laniakita@users.noreply.github.com>
2024-04-12 17:26:58 -1000

    add tailwindcss config

--- apps/divine-ahi/tailwind.config.ts
+++ apps/divine-ahi/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import sharedConfig from "@ahiakea/tailwindcss-config"
 
 const config: Config = {
   content: [
@@ -6,15 +7,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
+  presets: [sharedConfig],
   plugins: [],
 };
 export default config;
---
+++ packages/tailwindcss-config/tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+
+// We want each package to be responsible for its own content.
+const config: Omit<Config, "content"> = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
