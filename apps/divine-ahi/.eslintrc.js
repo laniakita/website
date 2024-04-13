9fd2c7aeae63ecf8d6d668efc143d0499a741943
laniakita <101172492+laniakita@users.noreply.github.com>
2024-04-12 17:15:41 -1000

    add tailwindcss eslint + prettier plugin, add prettier configs w tailwindcss plugin

--- apps/divine-ahi/.eslintrc.js
+++ apps/divine-ahi/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@ahiakea/eslint-config/next.js"],
+  extends: ["@ahiakea/eslint-config/next.js", "plugin:tailwindcss/recommended", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
