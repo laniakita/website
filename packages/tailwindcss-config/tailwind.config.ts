8132a2a3b58079d6cbc6209fdd4bb0575f59520f
laniakita <101172492+laniakita@users.noreply.github.com>
2024-04-12 19:33:53 -1000

    add catppuccin + tailwind prose

--- packages/tailwindcss-config/tailwind.config.ts
+++ packages/tailwindcss-config/tailwind.config.ts
@@ -1,4 +1,11 @@
 import type { Config } from "tailwindcss";
+import catppuccin from "@catppuccin/tailwindcss";
+import tailwindProse from "@tailwindcss/typography";
+
+const accent = "ctp-mauve";
+const invertAccent = "ctp-orange";
+const linkColor = "ctp-sky";
+const invertLinkColor = "ctp-orange";
 
 // We want each package to be responsible for its own content.
 const config: Omit<Config, "content"> = {
@@ -9,7 +16,54 @@ const config: Omit<Config, "content"> = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      typography: ({ theme }: { theme: any }) => ({
+        catppuccin: {
+          css: {
+            "--tw-prose-body": theme(`colors.ctp-text.DEFAULT`),
+            "--tw-prose-headings": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-lead": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-links": theme(`colors.${linkColor}.DEFAULT`),
+            "--tw-prose-bold": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-counters": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-bullets": theme(`colors.ctp-text.DEFAULT`),
+            "--tw-prose-hr": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-quotes": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-quote-borders": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-captions": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-code": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-pre-code": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-pre-bg": theme("colors.ctp-base.DEFAULT"),
+            "--tw-prose-th-borders": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-td-borders": theme(`colors.${accent}.DEFAULT`),
+            "--tw-prose-invert-body": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-invert-headings": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-invert-lead": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-invert-links": theme(
+              `colors.${invertLinkColor}.DEFAULT`,
+            ),
+            "--tw-prose-invert-bold": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-invert-counters": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-invert-bullets": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-invert-hr": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-invert-quotes": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-invert-quote-borders": theme(
+              `colors.${invertAccent}.DEFAULT`,
+            ),
+            "--tw-prose-invert-captions": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-invert-code": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-invert-pre-code": theme("colors.ctp-text.DEFAULT"),
+            "--tw-prose-invert-pre-bg": theme("rgb(0 0 0 / 50%)"),
+            "--tw-prose-invert-th-borders": theme(
+              `colors.${invertAccent}.DEFAULT`,
+            ),
+            "--tw-prose-invert-td-borders": theme(
+              `colors.${invertAccent}.DEFAULT`,
+            ),
+          },
+        },
+      }),
     },
   },
+  plugins: [tailwindProse, catppuccin({ prefix: "ctp" })],
 };
 export default config;
