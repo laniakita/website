7b1859e5e4012883b2c08982f6b79f946f837b6c
laniakita <101172492+laniakita@users.noreply.github.com>
2024-04-12 22:41:03 -1000

    feat: lite-next.config.mjs, re-added css from prev-repo

--- apps/divine-ahi/next.config.mjs
+++ apps/divine-ahi/next.config.mjs
@@ -1,12 +1,8 @@
 import { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } from 'next/constants.js';
-import withBundleAnalyzer from '@next/bundle-analyzer';
-
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   images: {
     formats: ['image/avif', 'image/webp'],
   },
@@ -14,7 +10,7 @@ const nextConfig = {
 };
 
 const nextConfigFunction = async (phase, { defaultConfig }) => {
-  const plugins = [bundleAnalyzer];
+  const plugins = [];
 
   if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
     const withSerwist = (await import('@serwist/next')).default({
@@ -23,13 +19,20 @@ const nextConfigFunction = async (phase, { defaultConfig }) => {
     });
     plugins.push(withSerwist);
 
-    const withMDX = (await import ('@next/mdx')).default({
+    const withMDX = (await import('@next/mdx')).default({
       options: {
-        remarkPlugins: [(await import('remark-gfm'))],
-        rehypePlugins: [(await import('@shikijs/rehype'))],
+        remarkPlugins: [(await import('remark-gfm')).default],
+        rehypePlugins: [(await import('@shikijs/rehype')).default],
       },
     });
-    plugins.push(withMDX)
+    plugins.push(withMDX);
+  }
+
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    const withBundleAnalyzer = (await import('@next/bundle-analyzer')).default({
+      enabled: process.env.ANALYZE === 'true',
+    });
+    plugins.push(withBundleAnalyzer)
   }
 
   return plugins.reduce((acc, next) => next(acc), {
--- apps/divine-ahi/src/app/page.tsx
+++ apps/divine-ahi/src/app/page.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className='ctp-mocha'>
       <div className='flex h-screen w-full items-center justify-center bg-ctp-sky text-ctp-text'>
         testing <span className='icon-[ph--alien-fill]' />
+        <div className="flex flex-row gap-4">
+          <Link href="/about">about</Link>
+          <Link href="/credits">credits</Link>
+          <Link href="/terms">terms</Link>
+          <Link href="/privacy">privacy</Link>
+        </div>
       </div>
       <article className='prose prose-catppuccin'>
         <p>this is some text</p>
