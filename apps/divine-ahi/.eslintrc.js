468e860144ac4fdd66ac4efc72d341164d156d8e
laniakita <101172492+laniakita@users.noreply.github.com>
2024-04-12 20:18:41 -1000

    configure postcss

--- apps/divine-ahi/.eslintrc.js
+++ apps/divine-ahi/.eslintrc.js
@@ -1,6 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
   extends: ["@ahiakea/eslint-config/next.js", "plugin:tailwindcss/recommended", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
--- apps/divine-ahi/tailwind.config.ts
+++ apps/divine-ahi/tailwind.config.ts
@@ -1,12 +1,20 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "@ahiakea/tailwindcss-config"
+import type { Config } from 'tailwindcss';
+import sharedConfig from '@ahiakea/tailwindcss-config';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   presets: [sharedConfig],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+        mono: ['var(--font-zeroxproto)'],
+      },
+    },
+  },
 };
 export default config;
