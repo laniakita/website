4cc576fd1863dce8240f55cea544db0ea43a2a74
laniakita <101172492+laniakita@users.noreply.github.com>
2024-04-14 22:47:55 -1000

    feat: serializer/deserializer to deal with categories with spaces

--- apps/divine-ahi/src/app/blog/[category]/page.tsx
+++ apps/divine-ahi/src/app/blog/[category]/page.tsx
@@ -4,41 +4,16 @@ import { getMDXComponent } from 'mdx-bundler/client';
 import { PostNumStoreProvider } from '@/providers/postnum-store-provider';
 import LoadingSpinner from '@/components/loading-spinner';
 import PreviewRollerV3 from '@/components/blog/post-roller-v3';
-import { queryCategoryDescr } from '@/lib/utils/mdxlite';
+import { queryCategoryDescr, queryPostsByCategory } from '@/lib/utils/mdxlite';
 import { resMdx } from '@/lib/utils/mdx-bundler-utils';
-
-
-//import path from 'node:path';
-//import process from 'node:process';
-//import { insertFromRawIndex } from '@/lib/utils/mdxlite';
-
-/*
-export const dynamicParams = false;
-export const revalidate = 86400;
-
-export async function generateStaticParams() {
-  const getCatKeys = await getAllCategoryKeys()
-  const getCategoryParams = await getAllCategoryTitles(getCatKeys as Array<_Object>);
-  if (!getCategoryParams) {
-    return
-  }
-  return getCategoryParams.map((title) => ({
-    category: title,
-  }));
-}
-*/
+import type { PostTeaserObjectProps } from '../page';
 
 export default async function Page({ params }: { params: { category: string } }) {
-  //const folder = path.resolve(process.cwd(), 'src/content')
-  //await insertFromRawIndex(folder)
-
-    
-
-  //const pageDataStr = await getSinglePost({ isDescr: true, category: params.category });
-  //const catDataArr = await getPostsV3({category: params.category});
-  const descrQ = await queryCategoryDescr(params.category);
+  const catDeserializer = params.category.replace('_', ' ')
+  const descrQ = await queryCategoryDescr(catDeserializer);
   const descrSource = descrQ?.rawContent?.trim();
-  const resBundle = descrSource && await resMdx(descrSource, 'category-info')
+  const resBundle = descrSource && (await resMdx(descrSource, 'category-info'));
+  const catDataArr = await queryPostsByCategory(catDeserializer)
 
   return (
     <Suspense fallback={<LoadingSpinner overrides='h-screen bg-black' />}>
@@ -49,14 +24,17 @@ export default async function Page({ params }: { params: { category: string } })
             <span>{`>`}</span>
             <Link href='/blog'>Blog</Link>
             <span>{`>`}</span>
-            <p>{params.category}</p>
+            <p>{catDeserializer}</p>
           </div>
           <div className='space-y-2'>
             <h3 className='font-mono text-xl font-semibold'>Posts tagged:</h3>
-            <h1 className=' text-5xl font-black uppercase md:text-6xl'>#{params.category}</h1>
+            <h1 className=' text-5xl font-black uppercase md:text-6xl'>#{catDeserializer}</h1>
           </div>
-          { resBundle ? <Description code={resBundle.code} /> : ''}
+          {resBundle ? <Description code={resBundle.code} /> : <div className='prose prose-catppuccin w-full max-w-2xl'><p>whoops, no posts found!</p></div>}
         </div>
+        <PostNumStoreProvider>
+          {catDataArr.length >= 1 && <PreviewRollerV3 isCat dataArr={catDataArr as PostTeaserObjectProps[]} />}
+        </PostNumStoreProvider>
       </div>
     </Suspense>
   );
@@ -87,9 +65,7 @@ function MDXtoJSX({ markdownString }: { markdownString: string }) {
 }
 
 
- *   <PostNumStoreProvider>
-          { catDataArr!.length >= 1 && <PreviewRollerV3 isCat={true} dataArr={catDataArr} /> }
-        </PostNumStoreProvider>
+ *   
 
 
 function MDXtoJSX({ markdownString }: { markdownString: string }) {
