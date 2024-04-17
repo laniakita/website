1adfa4800332703ec91e0b8342fd2980bdcd34e2
laniakita <101172492+laniakita@users.noreply.github.com>
2024-04-16 16:37:23 -1000

    add get posts

--- packages/mdxlite/bin/push.ts
+++ packages/mdxlite/bin/push.ts
@@ -3,6 +3,7 @@ import { parseArgs } from "util";
 import path from "node:path";
 import { readdir } from "node:fs/promises";
 import { blogPostFinder } from "../utils/read-folder";
+import matter from "gray-matter";
 
 const { values, positionals } = parseArgs({
   args: Bun.argv,
@@ -18,26 +19,50 @@ const { values, positionals } = parseArgs({
 console.log(values);
 console.log(positionals);
 
+/* phase 0: get local folder to read */
+
 const currentDir = process.cwd();
 const searchFolder = path.resolve(currentDir, values.content!);
 
-async function getPostPathsFromContentFolder(contentFolder: string) {
+/* phase 1: process local folder and get paths of MD & MDX files */
+
+// readdir version of blogPostFinder(), opendir with bun doesn't seem to support recursion yet
+async function getMDXPathsFromContentFolder(contentFolder: string) {
   const testDir = await readdir(contentFolder, {
     recursive: true,
   });
 
-  const newArr = await Promise.all(testDir.map(async (item) => {
-    const itemSplit = item.split("/");
-    const lastItem = itemSplit.pop();
-    const fileExt = lastItem?.split(".")[1];
-    if (fileExt === "mdx" || fileExt === "md") {
-      return item;
-    }
-  }))
+  const newArr = await Promise.all(
+    testDir.map(async (item) => {
+      const itemSplit = item.split("/");
+      const lastItem = itemSplit.pop();
+      const fileExt = lastItem?.split(".")[1];
+      if (fileExt === "mdx" || fileExt === "md") {
+        return item;
+      }
+    }),
+  );
+
   // need to purge undefined items
-  const finalArr = newArr.filter(el => el);
-  return finalArr
+  const finalArr = newArr.filter((el) => el);
+  return finalArr;
 }
 
+console.log(await getMDXPathsFromContentFolder(searchFolder));
 
-console.log(await getPostPathsFromContentFolder(searchFolder));
+/* phase 2: with file paths, we can send them into a database */
+async function mdxProcessor(dataArr: string[]) {
+  const fileArr = await Promise.all(
+    dataArr.map(async (mdxFilePath: string) => {
+      const blob = Bun.file(mdxFilePath);
+      const str = blob.toString();
+      const fileObj = {
+        meta: matter(str).data,
+        content: matter(str).content,
+        rawStr: str,
+      };
+      return fileObj;
+    }),
+  );
+  return fileArr;
+}
