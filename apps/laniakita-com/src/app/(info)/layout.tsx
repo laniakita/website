9fcc4ae2a171e509c8f22ba5cbba06e0b302be6d
laniakita <101172492+laniakita@users.noreply.github.com>
2024-04-20 01:15:05 -1000

    working emails, cleaned up about page, cleaned up other pages too

--- apps/laniakita-com/src/app/(info)/layout.tsx
+++ apps/laniakita-com/src/app/(info)/layout.tsx
@@ -2,10 +2,10 @@ import type { ReactNode } from 'react';
 
 export default function MarkdownLayout({ children }: { children: ReactNode }) {
   return (
-    <main className='simple-color-trans relative z-[5] -mb-1 min-h-screen  bg-ctp-base dark:bg-ctp-crust'>
+    <div className='simple-color-trans relative z-[5] -mb-1 min-h-full  bg-ctp-base dark:bg-ctp-crust'>
       <div className='flex items-center justify-center gap-6 p-10 lg:pb-20 lg:pt-36'>
         <div className='prose-protocol-omega'>{children}</div>
       </div>
-    </main>
+    </div>
   );
 }
