9690b25bd15683ecdabc34fc3193010ba83ab156
laniakita <101172492+laniakita@users.noreply.github.com>
2024-04-21 22:16:08 -1000

    add guidelines page and links to other agreement pages and replace mdx links with Link component

--- apps/laniakita-com/src/app/projects/page.tsx
+++ apps/laniakita-com/src/app/projects/page.tsx
@@ -1,9 +1,14 @@
-import ComingSoon from '@/components/coming-soon';
-
 export default function Page() {
   return (
-    <div className='py-40'>
-      <ComingSoon />
+    <div className='flex size-full h-screen max-h-[calc(100vh-3.9rem)] flex-col items-center justify-between md:flex-row lg:max-h-screen'>
+      <div className='flex size-full flex-col items-center justify-center bg-ctp-base'>
+        <h1 className='text-2xl font-black uppercase'>Dev Work</h1>
+        <p>Coming Soon</p>
+      </div>
+      <div className='flex size-full flex-col items-center justify-center  bg-ctp-crust'>
+        <h1 className='text-2xl font-black uppercase'>Artwork</h1>
+        <p>Coming Soon</p>
+      </div>
     </div>
   );
 }
--- apps/laniakita-com/src/mdx-components.tsx
+++ apps/laniakita-com/src/mdx-components.tsx
@@ -1,7 +1,12 @@
+import type {LinkProps} from 'next/link'
+import Link from 'next/link'
 import type { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    a: (props) => (
+      <Link {...props as LinkProps} />
+    ),
     ...components,
   };
 }
