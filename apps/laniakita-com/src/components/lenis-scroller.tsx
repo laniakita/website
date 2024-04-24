51c1429509f5e461e1992f9d90af52d19828b7cf
laniakita <101172492+laniakita@users.noreply.github.com>
2024-04-23 22:42:45 -1000

    clean up lenis component

--- apps/laniakita-com/src/components/lenis-scroller.tsx
+++ apps/laniakita-com/src/components/lenis-scroller.tsx
@@ -1,9 +1,9 @@
 'use client';
-import { addEffect, useFrame } from '@react-three/fiber';
+import { useFrame } from '@react-three/fiber';
 import { damp } from 'three/src/math/MathUtils.js';
-import { useEffect, useRef, type ReactNode } from 'react';
-import { ReactLenis, useLenis, Lenis } from '@/lib/lenis';
+import type { ReactNode } from 'react';
 import { useSearchParams } from 'next/navigation';
+import { ReactLenis, useLenis } from '@/lib/lenis';
 
 const state = {
   top: 0,
@@ -12,22 +12,12 @@ const state = {
 
 
 export function Scroller({ children }: { children: ReactNode }) {
-  //const lenisRef = useRef(null!);
 
   useLenis(({ scroll, progress }: { scroll: number; progress: number }) => {
     state.top = scroll;
     state.progress = progress;
   });
   
-  /*
-  useEffect(() => {
-    const effectSub = addEffect((time) => {
-      (lenisRef.current as unknown as LenisProps).lenis.raf(time);
-    });
-    return () => {
-      effectSub();
-    };
-  }, []);*/
 
   return (
     <ReactLenis options={{ orientation: 'vertical', gestureOrientation: 'both', syncTouch: true, touchInertiaMultiplier: 15, lerp: 0.2, syncTouchLerp: 0.09 }} root>
