eb498e3eb4afe2aec0784d3f93436feb8b65c13a
laniakita <101172492+laniakita@users.noreply.github.com>
2024-04-25 22:23:27 -1000

    shorks + credits update + respect reduced motion pref

--- apps/laniakita-com/src/app/page.tsx
+++ apps/laniakita-com/src/app/page.tsx
@@ -2,41 +2,49 @@
 /* eslint-disable react/no-unknown-property -- r3f */
 import { Suspense, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { View, PerspectiveCamera, Stars, Preload } from '@react-three/drei';
+import { DepthOfField, EffectComposer } from '@react-three/postprocessing';
+import { Stars } from '@react-three/drei';
+import { A11yAnnouncer, A11yUserPreferences } from '@react-three/a11y';
 import SceneOverlayV3 from '@/components/scene-overlay-alt';
 import Hajs from '@/components/canvas/scenes/haj2';
+import LoadingSpinner from '@/components/loading-spinner';
 
 export default function Page() {
   const ref = useRef(null!);
   return (
-    <main ref={ref} className='relative max-h-[calc(100dvh-4rem)] overflow-hidden  [height:_100dvh] lg:max-h-screen'>
+    <main ref={ref} className='relative overflow-hidden  [height:_100dvh] lg:max-h-screen'>
       <SceneOverlayV3 />
-      <View index={1} className='absolute size-full'>
-        <Suspense>
-          <PerspectiveCamera makeDefault position={[0, 0, 20]} fov={45} />
+      <Suspense fallback={<LoadingSpinner />}>
+        <Canvas
+          eventSource={ref}
+          flat
+          gl={{ antialias: false }}
+          dpr={[1, 1.5]}
+          camera={{ position: [0, 0, 20], fov: 40 }}
+          style={{
+            height: '100%',
+            width: '100%',
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            overflow: 'hidden',
+            pointerEvents: 'none',
+          }}
+        >
+          <A11yUserPreferences>
+            <Hajs />
+          </A11yUserPreferences>
           <Stars />
-          <Hajs />
           <spotLight position={[0, 40, 0]} intensity={10000} />
           <hemisphereLight intensity={1.5} />
-        </Suspense>
-      </View>
-      <Canvas
-        eventSource={ref}
-        style={{
-          height: '100%',
-          width: '100%',
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          overflow: 'hidden',
-          pointerEvents: 'none',
-        }}
-      >
-        <View.Port />
-        <Preload all />
-      </Canvas>
+          <EffectComposer>
+            <DepthOfField focusDistance={0.4} focalLength={0.6} bokehScale={4} height={680} />
+          </EffectComposer>
+        </Canvas>
+        <A11yAnnouncer />
+      </Suspense>
     </main>
   );
 }
--- apps/laniakita-com/src/components/canvas/scenes/haj2.tsx
+++ apps/laniakita-com/src/components/canvas/scenes/haj2.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-multi-assign -- it's easier this way  */
+import { A11y, useUserPreferences } from '@react-three/a11y';
 import { useFrame, useThree } from '@react-three/fiber';
 import { useRef, useState } from 'react';
-import type { Group, MeshStandardMaterial } from 'three'
+import type { Group, MeshStandardMaterial } from 'three';
 import { MathUtils, Color } from 'three';
 import { Shork, ShorkInstances } from '@/components/canvas/models/shork/shork';
 
@@ -37,18 +39,28 @@ function HajSetup({ z, speed, index }: { z: number; speed: number; index: number
     rX: Math.random() * Math.PI,
     rZ: Math.random() * Math.PI,
   });
+  const { a11yPrefersState } = useUserPreferences();
 
   useFrame((state, delta) => {
     // stops if not current tab
     if (delta < 0.1) {
-      ref.current?.position.set(index === 0 ? 0 : data.x * width, (data.y += delta * speed), -z);
+      // shork go up
+      ref.current?.position.set(
+        index === 0 ? 0 : data.x * width,
+        (data.y += a11yPrefersState.prefersReducedMotion ? 0 : delta * speed),
+        -z,
+      );
     }
+
     // rotate shork
-    ref.current?.rotation.set(
-      (data.rX += delta / data.spin),
-      Math.sin(index * 1000 + state.clock.elapsedTime / 10) * Math.PI,
-      (data.rZ += delta / data.spin),
-    );
+    if (!a11yPrefersState.prefersReducedMotion) {
+      ref.current?.rotation.set(
+        (data.rX += delta / data.spin),
+        Math.sin(index * 1000 + state.clock.elapsedTime / 10) * Math.PI,
+        (data.rZ += delta / data.spin),
+      );
+    }
+
     // reset shork to bottom of viewport
     if (data.y > height * (index === 0 ? 4 : 1)) {
       data.y = -(height * (index === 0 ? 4 : 1));
@@ -57,38 +69,41 @@ function HajSetup({ z, speed, index }: { z: number; speed: number; index: number
     if (data.x > width * (index === 0 ? 4 : 1)) {
       data.x = -(width * (index === 0 ? 4 : 1));
     }
-    
-    if (ref.current) {
-      // eslint-disable-next-line -- no multi assign
-      ref.current.scale.x = ref.current.scale.y = ref.current.scale.z = MathUtils.lerp(ref.current.scale.z, hovered ? 500 : 400, 0.1);
-    }
-    (ref.current?.children[0]?.children[0]?.children[0]?.children[0]?.children[0] as unknown as MeshStandardMaterial).color.lerp(
-      hajColor.set(hovered ? '#ea76cb' : 'white'),
-      hovered ? 1 : 0.1,
-    );
 
-    /*
-    .color.lerp(
-      hajColor.set(hovered ? 'red' : 'white'),
-      hovered ? 1 : 0.1,
-    ); */
+    if (ref.current && !a11yPrefersState.prefersReducedMotion) {
+      ref.current.scale.x =
+        ref.current.scale.y =
+        ref.current.scale.z =
+          MathUtils.lerp(ref.current.scale.z, hovered ? 500 : 400, 0.1);
+    } else if (ref.current) {
+      ref.current.scale.x =
+        ref.current.scale.y =
+        ref.current.scale.z =
+         hovered ? 500 : 400;
+    }
+    (
+      ref.current?.children[0]?.children[0]?.children[0]?.children[0]?.children[0] as unknown as MeshStandardMaterial
+    ).color.lerp(hajColor.set(hovered ? '#ea76cb' : 'white'), hovered ? 1 : 0.1);
   });
 
   return (
-    <group
-      ref={ref}
-      onPointerOver={() => {
-        setHovered(true);
-      }}
-      onPointerOut={() => {
-        setHovered(false);
-      }}
-    >
-      <ShorkInstances>
-        <Shork />
-      </ShorkInstances>
-    </group>
+    <>
+      {/* eslint-disable-next-line -- react-three a11y only gives a few roles */}
+      <A11y role='image' description='Astronaut Blahaj in Deep Space'>
+        <group
+          ref={ref}
+          onPointerOver={() => {
+            setHovered(true);
+          }}
+          onPointerOut={() => {
+            setHovered(false);
+          }}
+        >
+          <ShorkInstances>
+            <Shork />
+          </ShorkInstances>
+        </group>
+      </A11y>
+    </>
   );
 }
-
-// e.object.instanceKey.current.color.lerp(hajColor.set(new Color('white')), 0.5)}
