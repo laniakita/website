aa037bf2f01f9b15835b231ccfd209ce54e5f9c3
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-04 20:41:01 -1000

    update credits

--- apps/laniakita-com/src/components/scene-social-counter-overlay.tsx
+++ apps/laniakita-com/src/components/scene-social-counter-overlay.tsx
@@ -4,32 +4,32 @@ import { useHajClickerStore } from '@/providers/hajclicker-store-provider';
 import { socialItems } from './footer/footer';
 import TextSplitterUltra from './text-splitter-v2';
 
-export default function SocialCounterOverlay({model}:{model: string}) {
+export default function SocialCounterOverlay({ model }: { model: string }) {
   const { clickNum } = useHajClickerStore((state) => state);
   return (
     <>
       {clickNum >= 1 && (
-        <div className='ctp-mocha pointer-events-none absolute bottom-24 right-6 z-[2] flex w-fit touch-none flex-col items-end gap-2 -space-y-4 text-ctp-text  md:right-10 lg:bottom-10  '>
-          <p className='text-xl font-black uppercase'>{`${model} Click Counter`}</p>
-          <p className='h-[2.8rem] text-[2.3rem] font-black text-ctp-mauve'>{String(clickNum).padStart(8, '0')}</p>
+        <div className='ctp-mocha pointer-events-none absolute bottom-24 right-6 z-[2] flex w-fit touch-none flex-col items-end gap-2 -space-y-4 text-right  text-ctp-text md:right-10  lg:bottom-10'>
+          <p className='text-xl font-black uppercase'>{`${model} Clicker Counter`}</p>
+          <p className='text-3xl font-black text-ctp-mauve'>{String(clickNum).padStart(11, '0')}</p>
         </div>
       )}
       {clickNum < 1 && (
         <div className='ctp-mocha absolute bottom-20 right-6 z-[2] flex flex-col items-end gap-4 pb-4 text-ctp-text md:right-10 lg:bottom-10 lg:pb-0'>
-          <div className='pointer-events-none flex w-fit touch-none flex-col -space-y-2 '>
+          <div className='pointer-events-none flex w-fit touch-none flex-col -space-y-2 text-right'>
             <h3 className='overflow-hidden text-xl font-black uppercase'>
               <TextSplitterUltra
                 className='inline-flex'
-                textIn={`${model} Click Counter`}
+                textIn={`${model} Clicker Counter`}
                 spanRole='heading'
                 level={3}
                 charClass='motion-safe:animate-upDog [transform:_translateY(100%)] inline-block'
               />
             </h3>
-            <h3 className=' overflow-hidden text-[2.3rem] font-black uppercase'>
+            <h3 className='overflow-hidden text-3xl font-black uppercase'>
               <TextSplitterUltra
                 className='inline-flex'
-                textIn={String(clickNum).padStart(8, '0')}
+                textIn={String(clickNum).padStart(11, '0')}
                 spanRole='heading'
                 level={3}
                 charClass='motion-safe:animate-upDog [transform:_translateY(100%)] inline-block'
