e993d3e098ccbd2a30b5fd4e88956ecbc129b6a6
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-04 14:38:29 -1000

    move shark clicker to .old file, replace with robot

--- apps/laniakita-com/src/app/page.tsx
+++ apps/laniakita-com/src/app/page.tsx
@@ -1,112 +1,13 @@
-'use client';
-/* eslint-disable react/no-unknown-property -- r3f */
-import { Suspense, useEffect, useRef } from 'react';
-import dynamic from 'next/dynamic';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { DepthOfField, EffectComposer } from '@react-three/postprocessing';
-import { Stars } from '@react-three/drei';
-import { A11yAnnouncer, A11yUserPreferences, useUserPreferences } from '@react-three/a11y';
-import type { Points } from 'three';
-import LoadingSpinner from '@/components/loading-spinner';
-import { DeviceWidthStoreProvider, useDeviceWidthStore } from '@/providers/device-width-store-provider';
-import { useHajClickerStore } from '@/providers/hajclicker-store-provider';
-
-const SceneOverlayV3 = dynamic(() => import('@/components/scene-overlay-alt'), { ssr: false });
-
-const Hajs = dynamic(() => import('@/components/canvas/scenes/haj2'), { ssr: false });
-
-const HajClickerStoreProvider = dynamic(
-  () => import('@/providers/hajclicker-store-provider').then((mod) => mod.HajClickerStoreProvider),
-  { ssr: false },
-);
-
-const SocialCounterOverlay = dynamic(() => import('@/components/scene-social-counter-overlay'), { ssr: false });
+import BotClickerScene from '@/components/canvas/scenes/bot-clicker/main';
+import { DeviceWidthStoreProvider } from '@/providers/device-width-store-provider';
+import { HajClickerStoreProvider } from '@/providers/hajclicker-store-provider';
 
 export default function Page() {
   return (
     <HajClickerStoreProvider>
       <DeviceWidthStoreProvider>
-        <LandingScene />
+        <BotClickerScene />
       </DeviceWidthStoreProvider>
     </HajClickerStoreProvider>
-  );
-}
-
-function LandingScene() {
-  const ref = useRef(null!);
-  const windowRef = useRef<number>();
-  const { setMobile, setNotMobile, setTablet, setNotTablet } = useDeviceWidthStore((state) => state);
-  useEffect(() => {
-    const updateRef = () => {
-      windowRef.current = window.innerWidth;
-      if (windowRef.current < 768) {
-        setMobile();
-        setNotTablet();
-      } else if (windowRef.current >= 768) {
-        setTablet();
-        setNotMobile();
-      }
-    };
-    updateRef();
-    window.addEventListener('resize', updateRef);
-    return () => {
-      window.removeEventListener('resize', updateRef);
-    };
-  }, [setMobile, setNotMobile, setTablet, setNotTablet]);
-
-  return (
-    <main ref={ref} className='relative overflow-hidden  [height:_100dvh] lg:max-h-screen'>
-      <SceneOverlayV3 />
-      <SocialCounterOverlay />
-      <Suspense fallback={<LoadingSpinner />}>
-        <Canvas
-          eventSource={ref}
-          flat
-          gl={{ antialias: false }}
-          dpr={[1, 1.5]}
-          camera={{ position: [0, 0, 20], fov: 40 }}
-          style={{
-            height: '100%',
-            width: '100%',
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            overflow: 'hidden',
-            pointerEvents: 'none',
-          }}
-        >
-          <A11yUserPreferences>
-            <HajClickerMain />
-          </A11yUserPreferences>
-        </Canvas>
-        <A11yAnnouncer />
-      </Suspense>
-    </main>
-  );
-}
-
-function HajClickerMain() {
-  const starRef = useRef<Points>(null!);
-  const { a11yPrefersState } = useUserPreferences();
-  const { isMobile } = useDeviceWidthStore((state) => state);
-  const { clickNum } = useHajClickerStore((state) => state)
-  useFrame((state, delta) => {
-    if (delta < 0.1 && !a11yPrefersState.prefersReducedMotion) {
-      starRef.current.rotation.x += delta * 0.04;
-      starRef.current.rotation.y += delta * 0.03;
-    }
-  });
-  return (
-    <>
-      <Hajs speed={8} count={isMobile ? 40 : 80} />
-      <Stars ref={starRef} />
-      <spotLight position={[0, 40, 0]} intensity={10000} />
-      <hemisphereLight intensity={1.5} />
-      {!isMobile && clickNum < 1 && <EffectComposer enableNormalPass={false} multisampling={0}>
-        <DepthOfField worldFocusDistance={30} bokehScale={5} height={680} />
-      </EffectComposer>}
-    </>
   );
 }
