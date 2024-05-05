1833b958d34c9eeb19f616e0613b9f244a05e08a
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-04 20:46:54 -1000

    cleaning unusused files

--- apps/laniakita-com/src/helpers/components/three.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import type { ReactNode } from 'react';
-import r3f from '@/helpers/global';
-
-export default function Three({ children }: { children: ReactNode }) {
-  return <r3f.In>{children}</r3f.In>;
-}
