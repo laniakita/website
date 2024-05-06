fd265d0610470a9f13766d0a474feced5049582c
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-05 14:31:32 -1000

    fixed scroll bug with projects page

---
+++ apps/laniakita-com/src/app/(main)/projects/page.tsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: 'Projects',
+  description: "This is a catalogue of all the (officially released) cool stuff I'm working on.",
+  openGraph: {
+    title: 'Projects',
+    description: "This is a catalogue of all the (officially released) cool stuff I'm working on.",
+  },
+  twitter: {
+    card: 'summary',
+    title: 'Projects',
+    description: "This is a catalogue of all the (officially released) cool stuff I'm working on.",
+  },
+};
+
+export default function Projects() {
+  return (
+    <div className='flex h-screen w-full items-center justify-center'>
+      <h1 className='text-6xl'>Coming Soon</h1>
+    </div>
+  );
+}
