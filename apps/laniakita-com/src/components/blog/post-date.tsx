2b71d1dc5af39ecef9aa785c6e4fef0332a19e93
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-06 14:20:58 -1000

    fix timezone bug with ssg using client component to render the date on posts, and started work on projects page + bot clicker writeup

---
+++ apps/laniakita-com/src/components/blog/post-date.tsx
@@ -0,0 +1,19 @@
+'use client';
+import dayjs, { extend } from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import timezone from 'dayjs/plugin/timezone';
+
+extend(relativeTime);
+extend(localizedFormat);
+extend(timezone);
+
+export default function PostDate({dateStr}:{dateStr: string}) {
+  const postedDate = dayjs(dateStr).format('L LT');
+  return (
+    <p className='flex flex-row gap-2'>
+      <span>|</span>
+      {postedDate}
+    </p>
+  );
+}
