cf82eae65ab7bc4539b71785759311a818c891a0
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-08 23:25:46 -1000

    wip update to bulb

--- apps/laniakita-com/src/components/canvas/scenes/mandelbulb-port/mandelbulb-port.tsx
+++ apps/laniakita-com/src/components/canvas/scenes/mandelbulb-port/mandelbulb-port.tsx
@@ -1,20 +1,54 @@
 'use client';
 /* eslint-disable react/no-unknown-property -- jsx-eslint compatibility issues with r3f */
-import { useMemo } from 'react';
-import { MathUtils, Vector3 } from 'three';
+import { useMemo, useRef, useState } from 'react';
+import { MathUtils, type Points, Vector3 } from 'three';
+import { useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import CommonEmbedCanvas from '../../dom/common-embed-canvas';
 
+const updateVerts = (numVar: number) => {
+  // faking buffer sizes https://threejs.org/docs/#manual/en/introduction/How-to-update-things
+  const rawData = bulbSetup(numVar);
+  const bulb = { points: new Float32Array(rawData), needsUpdate: true }
+  return bulb;
+};
+
 export default function BulbPortMain() {
+  const [count, setCount] = useState(8);
+  const bulbVertices = useMemo(() => {
+    return updateVerts(count);
+  }, [count]);
+
   return (
-    <CommonEmbedCanvas camera={{position: [0,0,340], fov: 45}}>
-      <MandelbulbPort />
-      <OrbitControls />
-    </CommonEmbedCanvas>
+    <>
+      <div className='absolute right-4 top-4 z-[2] lg:right-10 lg:top-10'>
+        <button
+          onClick={() => {
+            count < 20 && setCount(count + 1);
+          }}
+          type='button'
+        >
+          +
+        </button>
+        <p>{count}</p>
+        <button
+          onClick={() => {
+            count > 1 && setCount(count - 1);
+          }}
+          type='button'
+        >
+          -
+        </button>
+      </div>
+      <CommonEmbedCanvas camera={{ position: [0, 0, 340], fov: 45 }}>
+        <MandelbulbPort bulb={bulbVertices} />
+        <OrbitControls />
+      </CommonEmbedCanvas>
+    </>
   );
 }
 
-function bulbSetup() {
+function bulbSetup(nVar: number) {
   class Spherical {
     r: number;
     theta: number;
@@ -33,10 +67,10 @@ function bulbSetup() {
     return new Spherical(r, theta, phi);
   };
   const DIM = 96;
-  const n = 8;
+  const n = nVar;
   const mandelbulb = [];
   let edge = false;
-  
+
   for (let i = 0; i < DIM; i++) {
     for (let j = 0; j < DIM; j++) {
       for (let k = 0; k < DIM; k++) {
@@ -79,19 +113,32 @@ function bulbSetup() {
       }
     }
   }
-  return mandelbulb
+  return mandelbulb;
 }
 
-function MandelbulbPort() {
-  const data = useMemo(() => bulbSetup(),[])
-  const bulbVertices = new Float32Array(data)
+function MandelbulbPort({bulb}: {bulb: { points: Float32Array, needsUpdate: boolean } }) {
+  const bulbRef = useRef<Points>(null!);
+
+  useFrame((state) => {
+    if (bulb.needsUpdate) {
+      const pos = bulbRef.current.geometry.getAttribute('position');
+      for (const i of bulb.points) {
+        // @ts-expect-error -- shouldn't be undefined
+        pos.array[i] = bulb.points[i];
+      }
+      bulbRef.current.geometry.setAttribute('position', pos);
+      bulbRef.current.geometry.attributes.position!.needsUpdate = true;
+      bulb.needsUpdate = false
+    }
+  });
+
   return (
-    <points>
+    <points ref={bulbRef}>
       <bufferGeometry>
-        <bufferAttribute 
+        <bufferAttribute
           attach='attributes-position'
-          count={bulbVertices.length/3}
-          array={bulbVertices}
+          count={bulb.points.length / 3}
+          array={bulb.points}
           itemSize={3}
         />
       </bufferGeometry>
