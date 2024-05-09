6794c4aa4c04beedf6cf28087f53fc53bab9283b
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-08 16:03:55 -1000

    add bot clicker image to public folder and add image with fallback for projects

--- apps/laniakita-com/src/app/projects/[slug]/page.tsx
+++ apps/laniakita-com/src/app/projects/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+import { getMDXComponent } from 'mdx-bundler/client';
 import { batchMatterFetch, fetchMdx } from '@/utils/mdx-utils';
 import { resMdxV3 } from '@/utils/mdx-bundler-utils';
 import type { WorkMetaProps } from '../page';
@@ -16,7 +18,36 @@ export default async function ProjectDetailPage({ params }: { params: { slug: st
   if (!data) return;
   // eslint-disable-next-line -- any is needed here to make mdx-bundler types happy
   const resData: { code: string; frontmatter: Record<string, any> } = await resMdxV3(data, folder, params.slug);
-  return <ClientProjPost code={resData.code} frontmatter={resData.frontmatter} />;
+  return (
+    <>
+      {(resData.frontmatter as WorkMetaProps).type === 'computer-graphics' ? (
+        <ClientProjPost code={resData.code} frontmatter={resData.frontmatter} />
+      ) : (
+        <DefaultProjPost code={resData.code} frontmatter={resData.frontmatter} />
+      )}
+      ;
+    </>
+  );
 }
 
-
+// eslint-disable-next-line -- any is needed here to make mdx-bundler types happy
+function DefaultProjPost({ code, frontmatter }: { code: string; frontmatter: Record<string, any> }) {
+  const Component = useMemo(() => getMDXComponent(code), [code]);
+  return (
+    <article className=''>
+      <header className='flex size-full flex-col items-center justify-center'>
+        <div className='flex w-full items-center justify-center px-10 pt-10 md:pt-20 lg:pb-[6.5rem] lg:pt-36'>
+          <div className='flex w-full max-w-xl flex-col gap-10'>
+            <h1 className='text-4xl font-black md:text-5xl'>{(frontmatter as WorkMetaProps).title}</h1>
+            <p className='prose-protocol-omega'>{(frontmatter as WorkMetaProps).descr}</p>
+          </div>
+        </div>
+      </header>
+      <div className='flex min-h-full items-center justify-center px-10 py-6'>
+        <div className='prose-protocol-omega w-full'>
+          <Component />
+        </div>
+      </div>
+    </article>
+  );
+}
---
+++ apps/laniakita-com/src/utils/image-loader.ts
@@ -0,0 +1,5 @@
+export const imageLoader = ({ src, width, quality }: { src?: string; width?: number; quality?: number }) => {
+  return `${src}?w=${width}&q=${quality ?? 50}`;
+};
+
+
