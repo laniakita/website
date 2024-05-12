e563fefe1c6b6a4d4958a63f98d51f51c21a7652
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-12 12:34:04 -1000

    projects post page now rendered via next-mdx

---
+++ apps/laniakita-com/src/app/projects/credits/layout.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from 'react';
+
+export default function MarkdownLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className='simple-color-trans relative z-[5] -mb-1 min-h-full  bg-ctp-base dark:bg-ctp-midnight'>
+      <div className='flex items-center justify-center gap-6 p-10 md:py-20 lg:pt-36'>
+        <div className='prose-protocol-omega'>{children}</div>
+      </div>
+    </div>
+  );
+}
