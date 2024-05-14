be323558232b7205d52936b5a1d8299de36c4ea0
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-13 20:28:16 -1000

    remove unused components remove unused global zustand stores

--- apps/laniakita-com/src/app/blog/hero-wrapper-v2.tsx
+++
@@ -1,8 +0,0 @@
-import { toBase64Blur } from '@/utils/blur-util';
-import type { PostTeaserObjectProps } from '@/utils/mdx-utils';
-import { ClientBg } from './client-hero-v2';
-
-export async function HeroWrapper({ dataObject }: { dataObject: PostTeaserObjectProps }) {
-  const heroRes = await toBase64Blur(dataObject.heroFile!);
-  return <ClientBg dataObject={dataObject} blurUrl={heroRes} />;
-}
