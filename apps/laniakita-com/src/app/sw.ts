4165ff4511c3a4857e1323a9f4ab5e33530c54a9
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-13 19:08:21 -1000

    prep for release candidate v0.99.01. feats: blur on demand to images, and also updated serwist, and added a sitemap.xml, and added a robots.txt, and updated the about page. whole site is using just the next/mdx package now, no external bundlers.

--- apps/laniakita-com/src/app/sw.ts
+++ apps/laniakita-com/src/app/sw.ts
@@ -1,7 +1,6 @@
-import type { SerwistGlobalConfig } from '@serwist/core';
 import { defaultCache } from '@serwist/next/worker';
-import { Serwist } from '@serwist/sw';
-import type { PrecacheEntry } from '@serwist/sw/precaching';
+import type { PrecacheEntry, SerwistGlobalConfig } from 'serwist';
+import { Serwist } from 'serwist';
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
@@ -14,12 +13,12 @@ declare global {
 
 declare const self: ServiceWorkerGlobalScope;
 
-const serwist = new Serwist();
-
-serwist.install({
+const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
   runtimeCaching: defaultCache,
 });
+
+serwist.addEventListeners();
