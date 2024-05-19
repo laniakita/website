18f675593000481545ff7e86587a7e357551395f
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-18 15:45:28 -1000

    recursive posts

--- apps/laniakita-com/src/app/blog/[slug]/page.tsx
+++ apps/laniakita-com/src/app/blog/[slug]/page.tsx
@@ -1,9 +1,10 @@
-import path from 'node:path';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment -- I'm not sure if you can type an MDX value */
 import type { Metadata, ResolvingMetadata } from 'next';
 import matter from 'gray-matter';
-import { batchMatterFetch, fetchFrontmatter, fetchMdx } from '@/utils/mdx-utils';
+import { batchMatterFetch, fetchMdx, fetchMdxPath } from '@/utils/mdx-utils';
 import { PostHeader } from '@/app/blog/post-header';
 import type { PostTeaserObjectProps } from '@/utils/mdx-utils';
+
 
 export async function generateStaticParams() {
   const postMetas = await batchMatterFetch('./src/app/blog/posts/published');
@@ -52,14 +53,27 @@ export async function generateMetadata(
 }
 
 export default async function BlogPostPage({ params }: { params: { slug: string } }) {
-  const filePath = path.resolve(process.cwd(), './src/app/blog/posts/published/', `${params.slug}.mdx`);
-  const matterData = await fetchFrontmatter(filePath);
-  //eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- I'm not sure if you can type an MDX value
-  const { default: MDXContent } = await import(`@/app/blog/posts/published/${params.slug}.mdx`);
+  //const filePath = path.resolve(process.cwd(), './src/app/blog/posts/published/', `${params.slug}.mdx`);
+  //const matterData = await fetchFrontmatter(filePath);
+  const getFile = await fetchMdx('./src/app/blog/posts/published', params.slug)
+  if (typeof(getFile) !== 'string') return;
+  const matterData = matter(getFile).data
+  const getPath = await fetchMdxPath('./src/app/blog/posts/published', params.slug)
+  if (!getPath) return;
+  const findPathIndent = getPath.split('/').map((segment, index) => {
+    if (segment === 'app') {
+      return index
+    }
+    return undefined;
+  });
+  const finalIndent = findPathIndent.filter((el) => el);
+  const modPath = getPath.split('/').slice(finalIndent[0]! + 2, getPath.length - 1).join('/')
+
+  const { default: MDXContent } = await import(`@/app/blog/${modPath}`);
 
   return (
     <main className='motion-safe:simple-color-trans -mb-0.5 min-h-full max-w-full bg-ctp-base dark:bg-ctp-midnight'>
-      {matterData !== undefined && (
+      {matterData as unknown !== undefined && (
         <article className=''>
           <PostHeader dataObject={matterData as PostTeaserObjectProps} />
           <div className='flex min-h-full items-center justify-center px-10 py-6'>
