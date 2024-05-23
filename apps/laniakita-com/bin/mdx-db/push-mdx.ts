9f55f9fdc2560dd0b0f2d7bdd9b6845e234ac522
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-22 23:03:55 -1000

    batch fetch and sort by priority

--- apps/laniakita-com/bin/mdx-db/push-mdx.ts
+++ apps/laniakita-com/bin/mdx-db/push-mdx.ts
@@ -1,13 +1,24 @@
 #! /usr/bin/env bun
 /* eslint-disable no-undef -- bun is bun */
 /* eslint-disable no-console -- bun is bun */
-
 import { type BatchFetchMain, batchFetchMain } from './fetch-mdx';
 
 export const batchPushMain = async (fetchConfig: BatchFetchMain) => {
   // get processed front matter array
   const matterRes = await batchFetchMain(fetchConfig);
-  console.log(matterRes)
+  console.log(matterRes);
+
+  if (!matterRes) {
+    console.log('Ooops, no data found!');
+    return;
+  }
+
+  // arr is sorted by priority so this should work:
+  const pushIn = matterRes.map((processedMDX) => {
+    if (processedMDX && 'type' in processedMDX) {
+      console.log('inserting', processedMDX.type)
+    }
+  })
+
+
 };
-
-
