1dd89a5dcfc39fd753c538e7e0c4a43110b3403f
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-25 12:43:50 -1000

    test db hotfix

--- apps/laniakita-com/drizzle.config.ts
+++ apps/laniakita-com/drizzle.config.ts
@@ -1,12 +1,13 @@
 import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
+import path from 'node:path';
 
 export default {
   schema: './src/lib/db/schema/*',
   out: './drizzle',
   dialect: 'sqlite',
   dbCredentials: {
-    url: 'sqlite-main.db',
+    url: path.join(process.cwd(), 'sqlite-main.db'),
   },
   verbose: true,
 } satisfies Config;
