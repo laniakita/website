257e8c9f77226bc5c6517811225fe6e553b55804
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-25 13:41:15 -1000

    update again

--- apps/laniakita-com/src/lib/db/bun-db.ts
+++ apps/laniakita-com/src/lib/db/bun-db.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-unresolved -- bun is provided by bun */
+import path from 'node:path';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { Database } from 'bun:sqlite';
 import * as authors from './schema/authors';
@@ -8,5 +9,5 @@ import * as posts from './schema/posts';
 
 // todo custom schemas via config file
 
-const sqlite = new Database('sqlite-main.db');
+const sqlite = new Database(path.join(process.cwd(), 'sqlite-main.db'));
 export const maindb = drizzle(sqlite, { schema: { ...authors, ...tags, ...featuredImages, ...posts } });
