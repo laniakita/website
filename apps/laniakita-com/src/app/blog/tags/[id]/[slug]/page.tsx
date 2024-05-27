28bbc82768eb5309e7468432d35bfb30e3a40f47
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-26 16:47:32 -1000

    better fix test

--- apps/laniakita-com/src/app/blog/tags/[id]/[slug]/page.tsx
+++ apps/laniakita-com/src/app/blog/tags/[id]/[slug]/page.tsx
@@ -3,14 +3,14 @@ import { redirect } from 'next/navigation';
 import { PostNumStoreProvider } from '@/providers/postnum-store-provider';
 import PreviewRollerV3 from '@/app/blog/post-roller-v3';
 import { getAllTags, getTag, getPostsWithTagID, type QueryPostMetaItem, type TagQ } from '@/lib/node-db-funcs';
-import { resMdxNoImgBundle } from '@/utils/mdxbundler-main';
+import { resMdxV3 } from '@/utils/mdxbundler-main';
 import descriptionHelper from '@/utils/description-helper';
 import { MdxJsx } from '../../../posts/[id]/[slug]/page';
 
 export async function generateStaticParams() {
   const tags = (await getAllTags()) as unknown as TagQ[];
   return tags.map((meta) => ({
-    id: meta.id,
+    id: meta.id.split('-').shift(),
     slug: meta.slug,
   }));
 }
@@ -59,7 +59,7 @@ export default async function TagPage({ params }: { params: { id: string; slug: 
   const rawMdx = firstQ.rawStr;
   if (!rawMdx) return;
   if (!cwdFolderStr) return;
-  const resMdx = await resMdxNoImgBundle(rawMdx, cwdFolderStr);
+  const resMdx = await resMdxV3(rawMdx, cwdFolderStr, firstQ.slug, 'tags');
 
   return (
     <div className='simple-color-trans relative z-[5] -mb-1  bg-ctp-base dark:bg-ctp-midnight'>
