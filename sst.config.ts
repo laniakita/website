4308f0c0c4747f41c41e2a2fe9085e4c7179a058
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-28 22:06:55 -1000

    domain

--- sst.config.ts
+++ sst.config.ts
@@ -1,11 +1,14 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
       name: "ahiakea",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
+      providers: {
+        aws: true,
+        cloudflare: true,
+      },
     };
   },
   async run() {
