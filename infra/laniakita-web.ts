5838633e5001ccea749fa7d41d0f4f200d3054dd
laniakita <101172492+laniakita@users.noreply.github.com>
2024-05-28 22:06:55 -1000

    domain

--- infra/laniakita-web.ts
+++ infra/laniakita-web.ts
@@ -1,7 +1,7 @@
 const secrets = {
   tursoUrl: new sst.Secret("TursoUrl"),
   tursoAuth: new sst.Secret("TursoAuth"),
-  sharpLayer: new sst.Secret("SharpLayer")
+  sharpLayer: new sst.Secret("SharpLayer"),
 };
 
 const allSecrets = Object.values(secrets);
@@ -11,6 +11,10 @@ export const laniakitaWeb = new sst.aws.Nextjs("LaniAkitaWeb", {
   openNextVersion: "3.0.2",
   link: [...allSecrets],
   buildCommand: "bun run open-next-build",
+  domain: {
+    name: "laniakita.com",
+    dns: sst.cloudflare.dns()
+  },
   transform: {
     server: (args) => {
       args.nodejs = {
