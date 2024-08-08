0b7e42b43d31bc64f858235b7716ad8041ee04de
Lani Akita <101172492+laniakita@users.noreply.github.com>
2024-08-07 21:26:18 -1000

    correct image loader for cloudfront

--- apps/laniakita-web/src/utils/image-loader.ts
+++ apps/laniakita-web/src/utils/image-loader.ts
@@ -1,3 +1,11 @@
-export const imageLoader = ({ src, width, quality }: { src?: string; width?: number; quality?: number }) => {
-  return `${src}?w=${width}&q=${quality ?? 50}`;
+import { BASE_URL } from "@/lib/constants";
+
+// Docs: https://aws.amazon.com/developer/application-security-performance/articles/image-optimization
+// cloudfrontLoader
+export const imageLoader = ({ src, width, quality }: { src: string; width: number; quality?: number }) => {
+  const url = new URL(`${BASE_URL}${src}`);
+  url.searchParams.set('format', 'auto');
+  url.searchParams.set('width', width.toString());
+  url.searchParams.set('quality', (quality ?? 75).toString());
+  return url.href;
 };
