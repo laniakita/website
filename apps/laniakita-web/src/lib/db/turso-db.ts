74d9ba8c11afafc8b45c46e616671d9b97794dce
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-19 16:38:38 -1000

    turso using env

--- apps/laniakita-web/src/lib/db/turso-db.ts
+++ apps/laniakita-web/src/lib/db/turso-db.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies -- false positive from sst */
 import * as dotenv from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
-import { Resource } from 'sst';
+//import { Resource } from 'sst';
 import { createClient } from '@libsql/client';
 import * as authors from './schema/authors';
 import * as tags from './schema/tags';
@@ -11,10 +11,10 @@ import * as posts from './schema/posts';
 dotenv.config();
 
 const client = createClient({
-  url: Resource.TursoUrl.value,
-  authToken: Resource.TursoAuth.value
-  //url: process.env.TURSO_DATABASE_URL!,
-  //authToken: process.env.TURSO_AUTH_TOKEN,
+  //url: Resource.TursoUrl.value,
+  //authToken: Resource.TursoAuth.value
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
 export const maindb = drizzle(client, { schema: { ...authors, ...tags, ...featuredImages, ...posts } });
