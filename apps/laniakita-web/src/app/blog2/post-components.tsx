1ba14c0904bc8718b99ad4e2c6f57edf87fce0f8
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-21 11:16:17 -1000

    process article teaser with mdxbundler

--- apps/laniakita-web/src/app/blog2/post-components.tsx
+++ apps/laniakita-web/src/app/blog2/post-components.tsx
@@ -1,12 +1,15 @@
 import localLoader from '@/lib/local-loader';
 import { imageLoader } from '@/utils/image-loader';
+import { resMdxNoImgBundle } from '@/utils/mdxbundler-main';
 import { type Post } from 'contentlayer/generated';
+import { getMDXComponent } from 'mdx-bundler/client';
 import Image from 'next/image';
+import { useMemo } from 'react';
 
 export default function PostRollerV4({ posts }: { posts: Post[] }) {
   return (
     <div className='flex items-center justify-center'>
-      <div className='flex w-full max-w-3xl flex-col gap-4 md:gap-6'>
+      <div className='flex w-full max-w-3xl flex-col gap-6 md:gap-10'>
         {posts.map((post, idx) => (
           <PostPreviewV4 key={idx} {...post} />
         ))}
@@ -15,9 +18,20 @@ export default function PostRollerV4({ posts }: { posts: Post[] }) {
   );
 }
 
-function PostPreviewV4(post: Post) {
+function MiniMDX({ code, frontmatter }: Record<string, any>) {
+  const MDXComponent = useMemo(() => getMDXComponent(code), [code]);
   return (
-    <div className='flex basis-full flex-col gap-4 border border-ctp-surface0 dark:border-ctp-base md:gap-6'>
+    <>
+      <MDXComponent />
+    </>
+  );
+}
+
+async function PostPreviewV4(post: Post) {
+  const descriptionStr = descriptionHelper(post.body.raw) as string;
+  const MDXStr = await resMdxNoImgBundle(descriptionStr, './content2');
+  return (
+    <div className='flex basis-full flex-col overflow-hidden rounded-md border border-ctp-surface0 dark:border-ctp-base'>
       <Image
         loader={
           localLoader
@@ -32,11 +46,16 @@ function PostPreviewV4(post: Post) {
         sizes='(max-width: 300px) 70vw, (max-width: 600px) 45vw, (max-width:1500px) 27vw'
         className='object-contain'
       />
-      <div className='px-4 md:px-6'>
-        <h2 className='text-3xl font-black leading-tight'>{post.headline}</h2>
-        <h3 className='text-2xl font-semibold leading-tight pt-2'>{post.subheadline}</h3>
+      <div className='p-6 md:p-10 flex flex-col gap-6'>
+        <div className=''>
+          <h2 className='text-balance text-3xl font-black'>{post.headline}</h2>
+          <h3 className='text-balance text-2xl font-light pt-2'>{post.subheadline}</h3>
+        </div>
+        <div className='w-full h-px bg-ctp-surface0 rounded' />
+        <div className='prose-protocol-omega max-w-full prose-p:my-0 text-pretty prose-p:text-base'>
+          <MiniMDX code={MDXStr.code} />
+        </div>
       </div>
-      <p className='px-4 pb-4 md:px-6 md:pb-6 -pt-2'>{descriptionHelper(post.body.raw)}</p>
     </div>
   );
 }
