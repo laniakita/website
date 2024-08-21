91caa13926f7643ef2a88799721dbf087c06f360
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-20 17:27:10 -1000

    add category and tags to contentlayer. bug fixes to handle nested images.

--- apps/laniakita-web/src/app/blog2/page.tsx
+++ apps/laniakita-web/src/app/blog2/page.tsx
@@ -3,7 +3,7 @@ import {compareDesc, format, parseISO} from 'date-fns'
 
 export default function BlogPage2 () {
   const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
-  console.log(posts)
+  console.dir(posts, {depth: null})
   return (
     <main>
     </main>
