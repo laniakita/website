cdffd7606e1530c8ae2fe992645b7356a0b09c45
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-21 21:16:28 -1000

    replace embedded mdx image with custom component

---
+++ apps/laniakita-web/src/app/blog2/cat-tag-roller.tsx
@@ -0,0 +1,49 @@
+
+import { allCategories, allTags } from 'contentlayer/generated';
+import Link from 'next/link';
+
+interface CatTag {
+  slug: string;
+}
+
+export function CatTagRoller({ cats, tags }: { cats?: string[] | undefined; tags?: string[] | undefined }) {
+  const categories = cats
+    ? cats.map((cat) => {
+        const category = allCategories.find(
+          (category) => category._raw.flattenedPath === `categories/${(cat as unknown as CatTag).slug}`,
+        );
+
+        return category;
+      })
+    : [];
+  const tagsArr = tags
+    ? tags.map((tagIdx) => {
+        const tag = allTags.find((tag) => tag._raw.flattenedPath === `tags/${(tagIdx as unknown as CatTag).slug}`);
+
+        return tag;
+      })
+    : [];
+
+  const comboArr = [...categories.sort((a,b) => a!.title!.localeCompare(b!.title!)), ...tagsArr.sort((a,b) => a!.title!.localeCompare(b!.title!))];
+
+  return (
+    <div className='flex flex-wrap'>
+      {comboArr
+        ? comboArr.map((combo, idx) => (
+            <p key={idx} className='font-mono'>
+              <Link href={`/${combo?._raw.flattenedPath}`}>
+                <span className='font-bold'>{`${combo?.type === 'Tag' ? '#' : ''}${combo?.title}`}</span>
+              </Link>
+              {comboArr[idx]?.type === 'Category' && comboArr[idx + 1]?.type === 'Tag' ? (
+                <span className='px-[1ch]'>{`|`}</span>
+              ) : (
+                idx < comboArr.length - 1 && <span className='pr-[1ch]'>{`,`}</span>
+              )}
+            </p>
+          ))
+        : ''}
+    </div>
+  );
+}
+
+
---
+++ apps/laniakita-web/src/app/blog2/img-blur-server.tsx
@@ -0,0 +1,38 @@
+import { type FC } from 'react';
+import { betterBlur } from '@/utils/better-blurs';
+import BlogImageBlur from './img-blur-client';
+
+export interface BlurClientProps {
+  src: string;
+  alt: string;
+  blur: string;
+  height: number;
+  width: number;
+}
+
+interface ImgProps {
+  src: string;
+  alt: string;
+}
+
+/*eslint-disable react/function-component-definition -- mdx bundlers component substitution type complains without this formatting */
+const BlogImageBlurServer: FC = async (props) => {
+  const blurRes = await betterBlur((props as ImgProps).src);
+  const propsObj = {
+    src: (props as ImgProps).src,
+    alt: (props as ImgProps).alt,
+    blur: blurRes.base64,
+    height: blurRes.height,
+    width: blurRes.width,
+  };
+  return (
+    <BlogImageBlur
+      src={propsObj.src}
+      alt={propsObj.alt}
+      blur={propsObj.blur}
+      height={propsObj.height}
+      width={propsObj.width}
+    />
+  );
+};
+export default BlogImageBlurServer;
