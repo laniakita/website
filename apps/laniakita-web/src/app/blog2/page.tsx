8398e23b9804a1d4b2af2d3265b15b86fb8ae449
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-22 13:26:36 -1000

    update eslint config turbo

--- apps/laniakita-web/src/app/blog2/page.tsx
+++ apps/laniakita-web/src/app/blog2/page.tsx
@@ -1,7 +1,7 @@
 import { allPosts, Post } from "contentlayer/generated";
 import {compareDesc, format, parseISO} from 'date-fns'
 import PostRollerV4 from "./post-components";
-import Sidebar from "./sidebar";
+import Sidebar from "@/components/sidebar/main";
 
 export default function BlogPage2 () {
   const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
