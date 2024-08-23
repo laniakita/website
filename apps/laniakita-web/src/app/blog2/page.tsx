2cf05fbb15562c16c64b16f1d7f8836a2678884e
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-23 08:33:08 -1000

    add common padding css class, create universal cat/tags route

--- apps/laniakita-web/src/app/blog2/page.tsx
+++ apps/laniakita-web/src/app/blog2/page.tsx
@@ -7,7 +7,7 @@ export default function BlogPage2() {
   const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
   //console.dir(posts, {depth: null})
   return (
-    <main className='simple-color-trans flex size-full flex-col-reverse justify-center gap-2 bg-ctp-base p-2 dark:bg-ctp-midnight md:flex-row md:gap-4 md:p-6 pt-[4.75rem] md:pt-24 lg:p-10 lg:pt-20'>
+    <main className='simple-color-trans common-padding flex size-full flex-col-reverse justify-center gap-2 bg-ctp-base dark:bg-ctp-midnight md:flex-row md:gap-4 '>
       <PostRollerV4 posts={posts} />
       <Sidebar />
     </main>
