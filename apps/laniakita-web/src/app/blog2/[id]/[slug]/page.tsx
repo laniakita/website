77d59d81bf707dfb6be35418e6ce26e4d55a880f
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-23 14:13:06 -1000

    function reading progress bar

--- apps/laniakita-web/src/app/blog2/[id]/[slug]/page.tsx
+++ apps/laniakita-web/src/app/blog2/[id]/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { notFound, redirect } from 'next/navigation';
 import { PostHeader2 } from '@/app/blog2/post-header-2';
 import { allPosts } from 'contentlayer/generated';
 import BlogImageBlurServer from '../../img-blur-server';
+import ReadingBar from '@/components/reading-bar';
 
 const mdxComponents = { img: BlogImageBlurServer };
 
@@ -24,15 +25,20 @@ export default function BlogPostPage({ params }: { params: { id: string; slug: s
   }
 
   return (
-    <main className='motion-safe:simple-color-trans -mb-0.5 min-h-full max-w-full bg-ctp-base dark:bg-ctp-midnight'>
-      <article id='content' className='flex size-full flex-col items-center justify-center'>
-        <PostHeader2 {...post} />
-        <div className='flex min-h-full items-center justify-center px-10 py-6'>
-          <div className='prose-protocol-omega max-w-3xl '>
-            <MDXContent code={post.body.code} components={mdxComponents} />
+    <>
+      <div className='fixed left-0 top-[3.8rem] z-50 w-full'>
+        <ReadingBar />
+      </div>
+      <main className='motion-safe:simple-color-trans -mb-0.5 min-h-full max-w-full bg-ctp-base dark:bg-ctp-midnight'>
+        <article id='content' className='flex size-full flex-col items-center justify-center'>
+          <PostHeader2 {...post} />
+          <div className='flex min-h-full items-center justify-center px-10 py-6'>
+            <div className='prose-protocol-omega max-w-3xl '>
+              <MDXContent code={post.body.code} components={mdxComponents} />
+            </div>
           </div>
-        </div>
-      </article>
-    </main>
+        </article>
+      </main>
+    </>
   );
 }
