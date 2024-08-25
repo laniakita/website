775b5b8b1387079fd8268e0bf4a6f611b23666c4
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-24 20:26:03 -1000

    wip move old mdx pages to content folder

--- apps/laniakita-web/src/app/tags/[slug]/page.tsx
+++ apps/laniakita-web/src/app/tags/[slug]/page.tsx
@@ -4,16 +4,12 @@ import { allTags, allPosts } from 'contentlayer/generated';
 import { MiniLayout } from '@/components/cat-tag-common';
 
 export default function TagPage({ params }: { params: { prefix: string; slug: string } }) {
-  if (params.slug === 'tags') {
-    const tag = allTags.find((tagX) => tagX.url.split('/').pop() === params.slug);
-    const matchingPosts = allPosts
-      .filter((postX) => postX.tags?.some((tagXP) => (tagXP as unknown as { slug: string }).slug === params.slug))
-      .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+  const tag = allTags.find((tagX) => tagX.url.split('/').pop() === params.slug);
+  const matchingPosts = allPosts
+    .filter((postX) => postX.tags?.some((tagXP) => (tagXP as unknown as { slug: string }).slug === params.slug))
+    .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
 
-    if (!tag) return notFound();
+  if (!tag) return notFound();
 
-    return <MiniLayout data={tag} posts={matchingPosts} />;
-  }
-
-  return notFound();
+  return <MiniLayout data={tag} posts={matchingPosts} />;
 }
