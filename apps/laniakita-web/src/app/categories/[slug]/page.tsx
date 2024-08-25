e94f64ae3ea7ae7d4644186ee0b6090e5bef396c
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-24 20:26:03 -1000

    wip move old mdx pages to content folder

--- apps/laniakita-web/src/app/categories/[slug]/page.tsx
+++ apps/laniakita-web/src/app/categories/[slug]/page.tsx
@@ -4,16 +4,12 @@ import { allCategories, allPosts } from 'contentlayer/generated';
 import { MiniLayout } from '@/components/cat-tag-common';
 
 export default function CategoryPage({ params }: { params: { slug: string } }) {
-  if (params.slug === 'categories') {
-    const category = allCategories.find((catX) => catX.url.split('/').pop() === params.slug);
-    const matchingPosts = allPosts
-      .filter((postX) => postX.categories?.some((cat) => (cat as unknown as { slug: string }).slug === params.slug))
-      .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+  const category = allCategories.find((catX) => catX.url.split('/').pop() === params.slug);
+  const matchingPosts = allPosts
+    .filter((postX) => postX.categories?.some((cat) => (cat as unknown as { slug: string }).slug === params.slug))
+    .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
 
-    if (!category) return notFound();
+  if (!category) return notFound();
 
-    return <MiniLayout data={category} posts={matchingPosts} />;
-  }
-  
-  return notFound();
+  return <MiniLayout data={category} posts={matchingPosts} />;
 }
