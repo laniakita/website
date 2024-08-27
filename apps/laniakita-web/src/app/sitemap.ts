4e8fb6dbe987ff8dff16d4b77e76f78234cb4cf9
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-26 15:10:42 -1000

    update sitemap. update atom.xml. & validated atom.xml with w3c.

--- apps/laniakita-web/src/app/sitemap.ts
+++ apps/laniakita-web/src/app/sitemap.ts
@@ -1,72 +1,108 @@
 import type { MetadataRoute } from 'next';
 import { compareDesc } from 'date-fns';
-import { allCategories, allPosts, allTags, allProjects, Post } from 'contentlayer/generated';
+import {
+  allCategories,
+  allPosts,
+  allTags,
+  allProjects,
+  type Tag,
+  type Category,
+  allPages,
+} from 'contentlayer/generated';
 import { BASE_URL } from '@/lib/constants';
-
-const baseSite = [
-  {
-    url: BASE_URL,
-    lastModified: new Date(),
-    changeFrequency: 'yearly',
-    priority: 1,
-  },
-  {
-    url: `${BASE_URL}/about`,
-    lastModified: new Date(),
-    changeFrequency: 'monthly',
-    priority: 0.8,
-  },
-  {
-    url: `${BASE_URL}/blog`,
-    lastModified: new Date(),
-    changeFrequency: 'weekly',
-    priority: 0.7,
-  },
-  {
-    url: `${BASE_URL}/projects`,
-    lastModified: new Date(),
-    changeFrequency: 'monthly',
-    priority: 0.6,
-  },
-  {
-    url: `${BASE_URL}/contact`,
-    lastModified: new Date(),
-    changeFrequency: 'yearly',
-    priority: 0.5,
-  },
-  {
-    url: `${BASE_URL}/credits`,
-    lastModified: new Date(),
-    changeFrequency: 'yearly',
-    priority: 0.2,
-  },
-];
 
 // funfact/improbable todo: google only supports 50,000 urls per sitemap, so filter
 // old content if needed.
 
+// eslint-disable-next-line @typescript-eslint/require-await -- return expects this to be async, so it's async now, albeit useless.
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-
   const postsRes = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
   const categoriesRes = allCategories.sort((a, b) => a.title!.localeCompare(b.title!));
   const tagsRes = allTags.sort((a, b) => a.title!.localeCompare(b.title!));
   const projectsRes = allProjects.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+  const isNewPostInCatTag = (cattag: Category | Tag, isCat: boolean) => {
+    // note: using postsRes above
+    if (isCat) {
+      const postResX = postsRes.find((postX) =>
+        postX.categories?.some(
+          (catX) => (catX as unknown as Category).slug === cattag._raw.flattenedPath.split('/').pop(),
+        ),
+      );
 
+      return postResX?.updated ?? postResX?.date ?? new Date();
+    }
+
+    const postResTag = postsRes.find((postY) =>
+      postY.tags?.some((tagX) => (tagX as unknown as Tag).slug === cattag._raw.flattenedPath.split('/').pop()),
+    );
+
+    return postResTag?.updated ?? postResTag?.date ?? new Date();
+  };
   
+  const getPageDate = (pageSlug: string) => {
+    const res = allPages.find((page) => page.url === `/${pageSlug}`);
+    return res?.date ?? new Date();
+  }
+
+  const baseSite = [
+    {
+      url: BASE_URL,
+      lastModified: postsRes[0]?.updated ?? postsRes[0]?.date ?? new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/blog`,
+      lastModified: postsRes[0]?.updated ?? postsRes[0]?.date ?? new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: `${BASE_URL}/about`,
+      lastModified: getPageDate('about'),
+      changeFrequency: 'yearly',
+      priority: 0.7,
+    },
+
+    {
+      url: `${BASE_URL}/projects`,
+      lastModified: projectsRes[0]?.updated ?? projectsRes[0]?.date ?? new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: `${BASE_URL}/contact`,
+      lastModified: getPageDate('contact'),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: `${BASE_URL}/credits`,
+      lastModified: getPageDate('credits'),
+      changeFrequency: 'yearly',
+      priority: 0.2,
+    },
+  ];
+
   const posts = postsRes.map((post) => ({
     url: `${BASE_URL}${post.url}`,
     lastModified: post.updated ?? post.date,
   }));
 
-  const categories = blogCategoriesMetas.map(({ id, slug, date }) => ({
-    url: `${BASE_URL}/${linker(id, slug!, 'blog/tags')}`,
-    lastModified: date ? date : new Date(),
+  const categories = categoriesRes.map((cat) => ({
+    url: `${BASE_URL}${cat.url}`,
+    lastModified: isNewPostInCatTag(cat, true),
   }));
 
-  const projects = projectMetas.map(({ slug, date }: WorkMetaProps) => ({
-    url: `${BASE_URL}/projects/${slug}`,
-    lastModified: date,
+  const tags = tagsRes.map((tag) => ({
+    url: `${BASE_URL}${tag.url}`,
+    lastModified: isNewPostInCatTag(tag, false),
   }));
 
-  return [...baseSite, ...projects, ...blogPosts, ...blogPostCategories];
+  const projects = projectsRes.map((project) => ({
+    url: `${BASE_URL}${project.url}`,
+    lastModified: project.updated ?? project.date,
+  }));
+
+  return [...baseSite, ...posts, ...categories, ...tags, ...projects];
 }
