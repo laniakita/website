1b72e318cd3d8b47fd89451f4151f4bff6905241
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-26 20:28:02 -1000

    added shader to projects

---
+++ apps/laniakita-web/src/app/projects/(three)/shaders/noise/01/page.tsx
@@ -0,0 +1,9 @@
+import NoiseShader01 from './noise';
+
+export default function NoiseShader1Page() {
+  return (
+    <main className='h-screen w-full'>
+      <NoiseShader01 />
+    </main>
+  );
+}
