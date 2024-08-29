631f961862622d01f093bb560d82f14443adbaf5
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-28 17:52:38 -1000

    re-add version vault to source control

--- apps/laniakita-web/.eslintrc.cjs
+++ apps/laniakita-web/.eslintrc.cjs
@@ -1,9 +1,13 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
+  plugins: [
+    'next-on-pages'
+  ],
   extends: [
     '@ahiakea/eslint-config/next.cjs',
     'plugin:@react-three/recommended',
     'plugin:tailwindcss/recommended',
+    'plugin:next-on-pages/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
