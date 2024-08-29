ae11d99858de575ff0fa38f68d00356ceb9650a6
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-28 17:37:54 -1000

    cleaning up unused

--- apps/laniakita-web/.tmp/src/components/coming-soon.tsx
+++
@@ -1,7 +0,0 @@
-export default function ComingSoon() {
-  return (
-    <div className='flex size-full items-center justify-center bg-ctp-base dark:bg-ctp-crust'>
-      <h1 className='text-6xl uppercase'>Coming Soon</h1>
-    </div>
-  );
-}
--- apps/laniakita-web/src/app/(content)/blog/twitter-image.tsx
+++ apps/laniakita-web/src/app/(content)/blog/twitter-image.tsx
@@ -4,8 +4,8 @@ import ImageGenTwo from '@/components/image-gen-two';
 export const runtime = 'edge';
 
 export default async function GET() {
-  const logoSrc = await fetch(new URL('../../laniakita-logo-transparent-darkmode.png', import.meta.url)).then(
-    (res) => res.arrayBuffer(),
+  const logoSrc = await fetch(new URL('../../laniakita-logo-transparent-darkmode.png', import.meta.url)).then((res) =>
+    res.arrayBuffer(),
   );
   const bgSrc = await fetch(new URL('../../noise_shader_01.png', import.meta.url)).then((res) => res.arrayBuffer());
   const interTightBlack = await fetch(new URL('../../InterTight-Black.ttf', import.meta.url)).then((res) =>
