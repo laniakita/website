fb91bb5b22fdc26ca75153451be449823206f5d7
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-28 21:32:15 -1000

    update ignore

--- apps/laniakita-web/.vercel/output/static/_next/static/chunks/app/(content)/layout-4cf2f83b071a824b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[948],{},function(n){n.O(0,[469,685,263,893,788,293,434,744],function(){return n(n.s=788)}),_N_E=n.O()}]);
--- apps/laniakita-web/.vercel/output/static/_next/static/chunks/app/not-found-ae0fe120763d7658.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{820:function(n,e,u){Promise.resolve().then(u.t.bind(u,5469,23))}},function(n){n.O(0,[469,293,434,744],function(){return n(n.s=820)}),_N_E=n.O()}]);
