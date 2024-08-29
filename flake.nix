48e8c31191552da90d249924a823f0c098045a7c
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-28 23:09:18 -1000

    retry flake

--- flake.nix
+++ flake.nix
@@ -16,19 +16,6 @@
   in {
     devShells = forAllSystems (pkgs: {
       default = pkgs.mkShell {
-        NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-          pkgs.stdenv.cc.cc
-          pkgs.openssl
-          pkgs.zlib
-          pkgs.fuse3
-          pkgs.icu
-          pkgs.nss
-          pkgs.openssl
-          pkgs.curl
-          pkgs.expat
-        ];
-        NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
-        NIX_LD_x86_64-linux = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
         packages = with pkgs; [
           turbo
           bun
