0a069f9afde70db9ac427bf3c7e89b41ded9ec69
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-29 18:06:42 -1000

    404 page added. home page update

--- apps/laniakita-web/src/app/(content)/page.tsx
+++ apps/laniakita-web/src/app/(content)/page.tsx
@@ -30,45 +30,44 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className='flex h-screen min-h-[40rem] w-full items-center justify-center'>
-      <div className='simple-color-trans absolute z-[2] flex size-full h-screen min-h-[40rem] items-center justify-center bg-ctp-midnight/10 dark:bg-ctp-midnight/40'>
-        <div className='simple-color-trans flex flex-col items-center justify-center gap-4  rounded-md bg-ctp-base/80 p-4'>
-          <div className='flex flex-col items-center justify-center'>
-            <h1 className='text-5xl font-black uppercase text-ctp-text'>Lani Akita</h1>
-            <h2 className='text-[1.45rem] font-semibold uppercase'>Full Stack Developer</h2>
-          </div>
-          <div className='flex w-full flex-col gap-3'>
-            <Link
-              href='/about'
-              className='color-trans-quick rounded border border-ctp-base bg-ctp-base/80 p-2 text-center font-mono text-lg text-ctp-text  backdrop-blur-md hover:border-ctp-pink hover:bg-ctp-pink hover:text-ctp-base  dark:border-ctp-surface0 dark:bg-ctp-midnight/70   dark:hover:border-ctp-flamingo dark:hover:bg-ctp-pink'
-            >
-              About Me
-            </Link>
+    <main className='relative flex h-screen min-h-[40rem] w-full items-center justify-center'>
+      <div className='absolute inset-0'>
+        <NoiseShader01 />
+      </div>
+      <div className='z-10 flex flex-col items-center justify-center gap-4 rounded-md border border-ctp-mauve bg-ctp-base/80 p-4 backdrop-blur-md md:p-10'>
+        <h1 className='text-5xl font-black uppercase text-ctp-text'>Lani Akita</h1>
+        <h2 className='text-[1.45rem] font-semibold uppercase'>Full Stack Developer</h2>
 
-            <Link
-              href='/projects'
-              className='color-trans-quick rounded border border-ctp-base bg-ctp-base/80 p-2 text-center font-mono text-lg text-ctp-text  backdrop-blur-md hover:border-ctp-pink hover:bg-ctp-pink hover:text-ctp-base  dark:border-ctp-surface0 dark:bg-ctp-midnight/70   dark:hover:border-ctp-flamingo dark:hover:bg-ctp-pink'
-            >
-              My Projects
-            </Link>
+        <div className='flex w-full flex-col gap-3'>
+          <Link
+            href='/about'
+            className='home-btn-standard'
+          >
+            About Me
+          </Link>
 
-            <Link
-              href='/blog'
-              className='color-trans-quick rounded border border-ctp-base bg-ctp-base/80 p-2 text-center font-mono text-lg text-ctp-text  backdrop-blur-md hover:border-ctp-pink hover:bg-ctp-pink hover:text-ctp-base dark:border-ctp-surface0 dark:bg-ctp-midnight/70  dark:hover:border-ctp-flamingo dark:hover:bg-ctp-pink'
-            >
-              Read the Blog
-            </Link>
+          <Link
+            href='/projects'
+            className='home-btn-standard'
+          >
+            My Projects
+          </Link>
 
-            <Link
-              href='/contact'
-              className='color-trans-quick rounded border border-ctp-mauve bg-ctp-mauve/10 p-2 text-center font-mono text-lg text-ctp-mauve   backdrop-blur-md  hover:border-ctp-surface0 hover:bg-ctp-pink hover:text-ctp-base dark:bg-ctp-mauve/30 dark:hover:border-ctp-flamingo dark:hover:bg-ctp-pink'
-            >
-              Contact Me
-            </Link>
-          </div>
+          <Link
+            href='/blog'
+            className='home-btn-standard'
+          >
+            Read the Blog
+          </Link>
+
+          <Link
+            href='/contact'
+            className='home-btn-extra'
+          >
+            Contact Me
+          </Link>
         </div>
       </div>
-      <NoiseShader01 />
     </main>
   );
 }
--- apps/laniakita-web/src/app/not-found.tsx
+++ apps/laniakita-web/src/app/not-found.tsx
@@ -1,14 +1,38 @@
 import Link from 'next/link';
+import { DarkStoreProvider } from '@/providers/theme-store-provider';
+import NavBar from '@/components/navbar/navbar';
+import Footer from '@/components/footer/footer';
+import NoiseShader01 from './projects/(three)/shaders/noise/01/noise';
 
 export default function NotFound() {
   return (
-    <main className='simple-color-trans size-full h-screen min-h-96 bg-ctp-base dark:bg-ctp-midnight'>
-      <div className='flex flex-col items-center justify-center rounded-md border border-ctp-surface0 p-8 dark:border-ctp-base'>
-        <h1 className='text-4xl font-black'>404</h1>
-        <h2 className='text-2xl'>Not Found.</h2>
-        <div className='w-full bg-ctp-surface0 dark:bg-ctp-base' />
-        <Link href='/'>back to home.</Link>
-      </div>
-    </main>
+    <>
+      <DarkStoreProvider>
+        <NavBar />
+      </DarkStoreProvider>
+
+      <main className='simple-color-trans relative flex size-full h-screen min-h-96 items-center justify-center bg-ctp-base dark:bg-ctp-midnight'>
+        <div className='absolute inset-0 z-0'>
+          <NoiseShader01 />
+        </div>
+
+        <div className='simple-color-trans z-10 flex w-fit flex-col items-center justify-center gap-4 rounded-md border border-ctp-mauve bg-ctp-base/80 p-8 backdrop-blur-md md:p-10'>
+          <div className='flex flex-col items-center justify-center'>
+            <h1 className='text-4xl font-black md:text-7xl'>404</h1>
+            <h2 className='text-2xl font-semibold md:text-3xl'>Not Found.</h2>
+          </div>
+          <div className='h-px w-full bg-ctp-text' />
+          <Link
+            href='/'
+            className='rounded border border-ctp-mauve px-4 py-2  font-mono no-underline [transition:_color_0.38s,_border_0.38s,_background-color_0.38s] hover:border-ctp-pink hover:bg-ctp-pink hover:text-ctp-base'
+          >
+            return home
+          </Link>
+        </div>
+      </main>
+      <DarkStoreProvider>
+        <Footer />
+      </DarkStoreProvider>
+    </>
   );
 }
