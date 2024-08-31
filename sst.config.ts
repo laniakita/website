beb8dbcb0f867cce3ae3f2f05066fdf3bd29f5bc
laniakita <101172492+laniakita@users.noreply.github.com>
2024-08-30 20:10:47 -1000

    update flake to add awscli2

---
+++ sst.config.ts
@@ -0,0 +1,19 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+export default $config({
+  app(input) {
+    return {
+      name: "website",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+      providers: {
+        aws: {
+          region: "us-west-2",
+        },
+        cloudflare: true,
+      },
+    };
+  },
+  async run() {
+    await import("./infra");
+  },
+});
