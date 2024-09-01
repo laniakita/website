45dd7de645d0e07ba34885b91af98f5f32d16b4f
laniakita <101172492+laniakita@users.noreply.github.com>
2024-09-01 12:19:25 -1000

    try this

--- apps/web/open-next.config.ts
+++ apps/web/open-next.config.ts
@@ -5,7 +5,6 @@ const config = {
   //buildCommand: 'exit 0', // in my example we set up Nx task distribution to handle the order of building.
   buildOutputPath: '.',
   appPath: '.',
-  packageJsonPath: '../../', // again, path to the root of your repo (where the package.json is)
   functions: {
     edge: {
       runtime: 'edge',
