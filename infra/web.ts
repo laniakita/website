64dd99d279946ce71b20f6688500535bf9f2259e
laniakita <101172492+laniakita@users.noreply.github.com>
2024-09-01 17:21:12 -1000

    edit config

--- infra/web.ts
+++ infra/web.ts
@@ -1,19 +1,10 @@
-const appUrl = new sst.Linkable("APP_URL", {
-  properties: {
-    url:
-      $app.stage === "production"
-        ? "https://laniakita.com"
-        : `https://${$app.stage}.laniakita.com`,
-  },
-});
-
 export const web = new sst.aws.Nextjs("Web", {
   path: "apps/web",
   openNextVersion: "3.1.1",
   buildCommand: "turbo build:open-next",
-  link: [appUrl],
   server: {
-    layers: [process.env.SHARP_ARN_01],
+    //layers: [process.env.SHARP_ARN_01],
+    install: ["sharp"],
   },
   environment: {
     NEXT_PUBLIC_DEPLOYED_URL:
