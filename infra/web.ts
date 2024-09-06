8cc8c5bf402785c777fe1f20b07fe3dde94a1543
laniakita <101172492+laniakita@users.noreply.github.com>
2024-09-05 21:19:51 -1000

    rm commented out code

--- infra/web.ts
+++ infra/web.ts
@@ -2,12 +2,6 @@ export const web = new sst.aws.Nextjs("Web", {
   path: "apps/web",
   openNextVersion: "3.1.1",
   buildCommand: "turbo copy-sharp",
-  /*
-  server: {
-    layers: [process.env.SHARP_ARN_01],
-    //install: ["sharp"],
-  },
-  */
   environment: {
     NEXT_PUBLIC_DEPLOYED_URL:
       $app.stage === "production"
