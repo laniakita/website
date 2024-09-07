1a07db38c2cd63416e3640f580c36793b4604f69
laniakita <101172492+laniakita@users.noreply.github.com>
2024-09-06 23:15:48 -1000

    updated

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -3,6 +3,18 @@
 import "sst"
 declare module "sst" {
   export interface Resource {
+    "BudgetAlertSNS": {
+      "arn": string
+      "type": "sst.aws.SnsTopic"
+    }
+    "KillSwitch": {
+      "name": string
+      "type": "sst.aws.Function"
+    }
+    "MonitorCloudFrontMetrics": {
+      "name": string
+      "type": "sst.aws.Function"
+    }
     "Web": {
       "type": "sst.aws.Nextjs"
       "url": string
