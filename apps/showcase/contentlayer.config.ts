747ffb629f5d497344023f21136fd44997243f22
laniakita <101172492+laniakita@users.noreply.github.com>
2024-10-09 15:39:18 -1000

    rename to showcase, add backlinks to main site

---
+++ apps/showcase/contentlayer.config.ts
@@ -0,0 +1,63 @@
+import { defineDocumentType, makeSource } from 'contentlayer2/source-files';
+import { imageProcessor, FeaturedImageR1 } from './src/lib/image-process';
+
+const CONTENT_DIR = 'content';
+
+export const Project = defineDocumentType(() => ({
+  name: 'Project',
+  filePathPattern: 'projects/**/*.yaml',
+  contentType: 'data',
+  fields: {
+    id: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+    updated: { type: 'date', required: false },
+    title: { type: 'string', required: true },
+    tech: {
+      type: 'list',
+      of: { type: 'string' },
+    },
+    imageSrc: { type: 'string', required: false },
+    altText: { type: 'string', required: false },
+    caption: { type: 'string', required: false },
+    description: { type: 'string', required: true },
+    blogPost: { type: 'string', required: false },
+    link: { type: 'string', required: false },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (project) => `/${project._raw.flattenedPath}`,
+    },
+    featured_image: {
+      type: 'json',
+      resolve: async (project): Promise<FeaturedImageR1> => {
+        if (!project.imageSrc) return new FeaturedImageR1(false, '', '', 0, 0, '', '', '', null);
+        const data = await imageProcessor({
+          contentDir: CONTENT_DIR,
+          prefix: `${CONTENT_DIR}/${project._raw.flattenedPath}`,
+          imgPath: project.imageSrc,
+          debug: false,
+        });
+
+        const res = new FeaturedImageR1(
+          true,
+          data.src,
+          data.base64,
+          data.height,
+          data.width,
+          data.resized,
+          (project.altText) ?? '',
+          (project.caption) ?? '',
+          data._debug ?? null,
+        );
+
+        return res;
+      },
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: CONTENT_DIR,
+  documentTypes: [Project],
+});
---
+++ apps/showcase/src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const BASE_URL = 'http://localhost:3100';
+export const APP_URL =
+  process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_DEPLOYED_URL
+    ? process.env.NEXT_PUBLIC_DEPLOYED_URL
+    : BASE_URL;
+export const APP_NAME = 'Lani Akita';
+export const APP_DEFAULT_TITLE = 'Lani Akita';
+export const APP_TITLE_TEMPLATE = '%s - Lani Akita';
+export const APP_DESCRIPTION = "Lani Akita's personal website";
+export const APP_THEME_COLOR = '#11111b';
+export const BLOG_DESCR = 'A blog about life, Linux, and web development. Written by, Lani Akita.';
+export const RESUME_LINK = '/lani-akita_resume-october-2024.pdf';
+export const MAIN_SITE_URL = process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_DEPLOYED_URL ? 'https://laniakita.com' : 'http://localhost:3000'
