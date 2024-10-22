af4ec47c90cee1a36caaaf70f1dcd2338f18dc18
laniakita <101172492+laniakita@users.noreply.github.com>
2024-10-22 13:27:10 -1000

    updated redirects

--- apps/web/src/lib/constants-js.mjs
+++ apps/web/src/lib/constants-js.mjs
@@ -9,7 +9,7 @@ export const APP_TITLE_TEMPLATE = '%s - Lani Akita';
 export const APP_DESCRIPTION = "Lani Akita's personal website";
 export const APP_THEME_COLOR = '#11111b';
 export const BLOG_DESCR = 'A blog about life, Linux, and web development. Written by, Lani Akita.';
-export const RESUME_LINK = '/lani-akita_resume-october-2024.pdf';
+export const RESUME_LINK = '/lani-akita_resume-november-2024.pdf';
 export const SHOWCASE_URL =
   process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_DEPLOYED_URL
     ? 'https://showcase.laniakita.com'
