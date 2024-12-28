6e9f47d24848128e0dd8483d6cd56bd943aeb9ac
laniakita <101172492+laniakita@users.noreply.github.com>
2024-12-28 12:07:59 -1000

    intersection observer to highlight current section title

--- apps/web/src/components/toc-nav.tsx
+++ apps/web/src/components/toc-nav.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
 
 // inspired by Emma Goto React ToC: https://www.emgoto.com/react-table-of-contents
 
@@ -57,7 +57,7 @@ const useHeadingsData = () => {
     };
 
     // filtering by id length excises the sub-headline from the array
-    const headingEls = Array.from(document.querySelectorAll('h2, h3, h4, h5, h6, h7')).filter((el) => el.id.length > 0);
+    const headingEls = Array.from(document.querySelectorAll('h2, h3, h4, h5, h6')).filter((el) => el.id.length > 0);
 
     const nested = getNestedHeadings(headingEls as HTMLHeadingElement[], 2);
 
@@ -67,46 +67,112 @@ const useHeadingsData = () => {
   return { nestedHeadings };
 };
 
-function HeadingNode({ node }: { node: HeadingNode }) {
+function HeadingNode({ node, activeId }: { node: HeadingNode; activeId: string }) {
+  console.log('nodeid:', node.id, 'activeId:', activeId);
   return (
-    <li key={node.id} className=''>
+    <li key={node.id}>
       <p className='text-balance'>
         <Link
           href={`#${node.id}`}
-          className='text-balance font-mono text-sm leading-relaxed text-ctp-subtext0 hover:text-ctp-text md:max-w-xs lg:max-w-sm md:break-words lg:break-keep'
+          className={`text-balance font-mono text-sm leading-relaxed text-ctp-subtext0 hover:text-ctp-text ${activeId === node.id ? 'text-ctp-text underline' : ''} md:max-w-xs md:break-words lg:max-w-sm lg:break-keep`}
         >
           {node.title}
         </Link>
       </p>
       <ul className='list-none pl-[2ch]'>
-        {node.children && node.children.map((childNode) => <HeadingNode key={childNode.id} node={childNode} />)}
+        {node.children && node.children.map((childNode) => <HeadingNode key={childNode.id} node={childNode} activeId={activeId} />)}
       </ul>
     </li>
   );
 }
 
-function Headings({ tree }: { tree: HeadingNode[] }) {
+function Headings({ tree, activeId }: { tree: HeadingNode[]; activeId: string}) {
   return (
     <ul className='list-none leading-relaxed'>
       {tree.map((heading) => (
-        <HeadingNode key={heading.id} node={heading} />
+        <HeadingNode key={heading.id} node={heading} activeId={activeId} />
       ))}
     </ul>
   );
 }
 
-const useIntersectionObserver = () => {
-  useEffect(() => {});
+const useIntersectionObserver = (setActiveId: Dispatch<SetStateAction<string>>, activeId: string) => {
+  const headingElsRef = useRef<Record<string, IntersectionObserverEntry>>({});
+
+  useEffect(() => {
+    const callback = (headings: IntersectionObserverEntry[]) => {
+      //console.log('running callback with', headings)
+      
+      headingElsRef.current = headings.reduce<Record<string, IntersectionObserverEntry>>((map, headingEl) => {
+        //console.log('current map:', map)
+        map[headingEl.target.id] = headingEl;
+        return map;
+      }, headingElsRef.current);
+
+      const visibleHeadings: IntersectionObserverEntry[] = [];
+      //console.log('current visible headings:', visibleHeadings)
+
+      if (headingElsRef.current) {
+        //console.log('current ref inside object-keys:', headingElsRef.current);
+        Object.keys(headingElsRef.current).forEach((key) => {
+          const headingEl = headingElsRef.current![key];
+          if (headingEl?.isIntersecting) visibleHeadings.push(headingEl);
+          //console.log('current visible headings after push:', visibleHeadings)
+
+        });
+      }
+
+      const getIndexFromId = (id: string): number => {
+        //console.log('searching for index with id:', id);
+        return headingEls.findIndex((heading) => heading.id === id);
+      };
+
+      if (visibleHeadings.length === 0) {
+        //console.log('visbile heading length === 0');
+
+        const activeElement = headingEls.find((el) => el.id === activeId);
+        const activeIndex = headingEls.findIndex((el) => el.id === activeId);
+
+        const activeIdYcoord = activeElement?.getBoundingClientRect().y;
+        if (activeIdYcoord && activeIdYcoord > 150 && activeIndex !== 0) {
+          setActiveId(headingEls[activeIndex - 1]!.id);
+
+          //console.log('active id set:', headingEls[activeIndex - 1]!.id);
+        }
+      } else if (visibleHeadings.length === 1) {
+        //console.log('visbile heading length === 1');
+        setActiveId(visibleHeadings[0]!.target.id);
+        //console.log('active id set:', visibleHeadings[0]!.target.id);
+      } else if (visibleHeadings.length > 1) {
+        //console.log('visbile heading length > 0');
+        const sortedVisibleHeadings = visibleHeadings.sort(
+          (a, b) => getIndexFromId(a.target.id) - getIndexFromId(b.target.id),
+        );
+        setActiveId(sortedVisibleHeadings[0]!.target.id);
+        //console.log('active id set:', sortedVisibleHeadings[0]!.target.id);
+      }
+    };
+
+    const observer = new IntersectionObserver(callback, {
+      rootMargin: '-72px 0px -40% 0px',
+    });
+
+    const headingEls = Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6'));
+    headingEls.forEach((el) => observer.observe(el));
+
+    return () => observer.disconnect();
+  }, [setActiveId, activeId]);
 };
 
 export default function ToCMenu() {
+  const [activeId, setActiveId] = useState('');
   const { nestedHeadings } = useHeadingsData();
-  console.log('filtered:', nestedHeadings);
-
+  useIntersectionObserver(setActiveId, activeId);
+  //console.log('currently active should be:', activeId);
   return (
-    <div className='flex h-screen max-h-[calc(100vh-4rem)] w-full min-w-[22rem] md:max-w-xs lg:max-w-sm py-10 items-start justify-center overflow-y-auto bg-ctp-base/20  text-slate-100 sticky top-16 dark:bg-ctp-base/20'>
+    <div className='sticky top-16 flex h-screen max-h-[calc(100vh-4rem)] w-full min-w-[22rem] items-start justify-center overflow-y-auto bg-ctp-base/20 py-10 text-slate-100 md:max-w-xs lg:max-w-sm dark:bg-ctp-base/20'>
       <nav aria-label='Table of contents' className='w-full px-4'>
-        <Headings tree={nestedHeadings as HeadingNode[]} />
+        <Headings tree={nestedHeadings as HeadingNode[]} activeId={activeId} />
       </nav>
     </div>
   );
