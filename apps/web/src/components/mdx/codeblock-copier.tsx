ba361e75b90260963644188a63a8a425f0334497
laniakita <101172492+laniakita@users.noreply.github.com>
2024-12-29 16:19:48 -1000

    animation for codeblock expand. handled edgecases for codeblock copy button

--- apps/web/src/components/mdx/codeblock-copier.tsx
+++ apps/web/src/components/mdx/codeblock-copier.tsx
@@ -1,21 +1,34 @@
 'use client';
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, Suspense, useEffect, useId, useRef, useState } from 'react';
 
 export default function CodeBlockCopier(
   props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLPreElement>, HTMLPreElement>,
 ) {
-  const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <Suspense>
+      <CodeBlockAssemble {...props} />
+    </Suspense>
+  );
+}
 
-  interface PreCodeBlock extends ReactElement {
-    props: {
-      children: Object[];
-    };
-  }
-
+function CodeBlockAssemble(props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLPreElement>, HTMLPreElement>) {
   if ((props.children as ReactElement).type === 'code') {
     const inputBlock = props.children as PreCodeBlock;
-    if (inputBlock.props.children.length > 30) {
-      //console.log(inputBlock)
+    interface PreCodeBlock extends ReactElement {
+      props: {
+        children: Object[];
+      };
+    }
+
+    // string is a weird edge case here.
+    if (inputBlock.props.children.length > 30 && typeof inputBlock.props.children !== 'string') {
+      const [isExpanded, setIsExpanded] = useState(false);
+      const [codeHeight, setCodeHeight] = useState(0);
+
+      const codeBlockRef = useRef<HTMLDivElement>(null!);
+      const preRef = useRef<HTMLPreElement>(null!);
+      const overlayRef = useRef<HTMLDivElement>(null!);
+
       const collapsedBlock = {
         ...inputBlock,
         props: {
@@ -24,41 +37,87 @@ export default function CodeBlockCopier(
         },
       };
 
+      const blockSerial = useId();
+      const blockId = `codeblock-${blockSerial}`;
+      const preId = `pre-${blockId}`;
+      //console.log(inputBlock.props.children.length, blockId);
+
+      useEffect(() => {
+        // init codeHeight
+        if (codeHeight <= 0 && codeBlockRef.current.offsetHeight > codeHeight) {
+          setCodeHeight(codeBlockRef.current.offsetHeight);
+        }
+        codeBlockRef.current.style.height = codeHeight + 'px';
+      }, [isExpanded, codeHeight]);
+
+      const handleExpand = () => {
+        setIsExpanded(true);
+        // get expanded height;
+        // timeout necessary, otherwise it uses the collapsed height
+        setTimeout(() => {
+          //console.log('await getting height');
+          setCodeHeight(preRef.current.offsetHeight);
+        }, 50);
+      };
+
       return (
-        <div className={`relative ${isExpanded ? '' : ''}`}>
-          <pre {...props}>
-            <button
-              onClick={() => setIsExpanded(!isExpanded)}
-              className='absolute right-4 top-4 flex items-center justify-center rounded-md border border-ctp-overlay0 bg-ctp-mantle/20 p-2 text-center text-ctp-overlay0 backdrop-blur'
-            >
-              <span className='icon-[ph--clipboard-text] text-2xl' />
+        <div
+          ref={codeBlockRef}
+          id={blockId}
+          className='relative my-[1.5rem] overflow-y-hidden [transition:_height_0.8s_ease]'
+        >
+          <div ref={overlayRef} className='pointer-events-none absolute inset-0'>
+            <button className='pointer-events-auto absolute right-4 top-4 flex items-center justify-center rounded-md border border-ctp-overlay0 bg-ctp-mantle/20 p-2 text-center text-ctp-overlay1 backdrop-blur'>
+              <span className='icon-[ph--clipboard-text] h-[1.5rem] w-[1.5rem]' />
             </button>
+
             <div
-              className={`absolute ${isExpanded ? 'hidden' : ''} inset-x-0 bottom-0 flex h-full items-center justify-center rounded-b-lg bg-ctp-mantle/20 bg-gradient-to-b from-transparent via-transparent to-ctp-midnight text-center text-ctp-overlay0`}
+              className={`absolute ${isExpanded ? 'hidden' : ''} inset-x-0 bottom-0 flex h-full max-h-[50%] w-full items-center justify-center overflow-x-auto rounded-b-lg bg-ctp-mantle/20 bg-gradient-to-b from-transparent to-ctp-base text-center text-ctp-overlay0 dark:to-ctp-midnight`}
             >
-              <button 
-              
-              onClick={() => setIsExpanded(!isExpanded)}
-              className='w-fit bg-ctp-sapphire flex flex-row items-center p-4 rounded-lg'>
-                <span className='icon-[ph--clipboard-text] text-2xl' />
-                show code
+              <button
+                onClick={() => handleExpand()}
+                className='pointer-events-auto flex w-fit flex-row items-center rounded-lg border border-ctp-mauve bg-ctp-mauve/10 p-4 text-ctp-mauve shadow-lg backdrop-blur-sm [transition:_color_0.3s,_border_0.3s,_box-shadow_0.3s,_backdrop-filter_0.3s,_background_0.3s] hover:border-ctp-pink hover:bg-ctp-pink/10 hover:text-ctp-pink hover:shadow-xl hover:backdrop-blur'
+              >
+                {`</> show code`}
               </button>
             </div>
+          </div>
+
+          <pre ref={preRef} id={preId} className='my-0' {...props}>
             {isExpanded ? props.children : collapsedBlock}
           </pre>
         </div>
       );
     }
+
+    // handle weird singleline blocks
+    const nonCollapsedPreRef = useRef<HTMLPreElement>(null!);
+    const [topPos, setTopPos] = useState('top-4');
+
+    useEffect(() => {
+      console.log()
+      if (nonCollapsedPreRef.current.childNodes.length == 2 && nonCollapsedPreRef.current.childNodes[1]?.childNodes) {
+        if (nonCollapsedPreRef.current.childNodes[1]?.childNodes.length <= 3) {
+          if(nonCollapsedPreRef.current.childNodes[1]?.childNodes[0]?.nodeName === "DIV") {
+            if( nonCollapsedPreRef.current.childNodes[1]?.childNodes.length === 1) {
+              setTopPos('top-2');
+            }
+          }
+        }
+      }
+    },[])
+
+    return (
+      <div className='relative'>
+        <pre ref={nonCollapsedPreRef} {...props}>
+        <button className={`pointer-events-auto absolute right-4 ${topPos} flex items-center justify-center rounded-md border border-ctp-overlay0 bg-ctp-mantle/20 p-1 text-center text-ctp-overlay1 backdrop-blur`}>
+              <span className='icon-[ph--clipboard-text] h-[1.5rem] w-[1.5rem]' />
+            </button>
+          {props.children}
+        </pre>
+      </div>
+    );
   }
 
-  return (
-    <div className='relative'>
-      <pre {...props}>
-        <button className='absolute right-4 top-4 flex items-center justify-center rounded-md border border-ctp-overlay0 bg-ctp-mantle/20 p-2 text-center text-ctp-overlay0 backdrop-blur'>
-          <span className='icon-[ph--clipboard-text] text-2xl' />
-        </button>
-        {props.children}
-      </pre>
-    </div>
-  );
+  return <pre {...props} />;
 }
