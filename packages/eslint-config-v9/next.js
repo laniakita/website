ede72d74b5c72da1601ca920f828909d40b9a834
laniakita <101172492+laniakita@users.noreply.github.com>
2025-01-27 21:47:58 -1000

    refactor: remove tailwind-eslint for now

--- packages/eslint-config-v9/next.js
+++ packages/eslint-config-v9/next.js
@@ -18,29 +18,8 @@ import { config as baseConfig } from "./base.js";
 export const nextJsConfig = [
   ...baseConfig,
   js.configs.recommended,
-  ...tailwind.configs["flat/recommended"],
-  {
-    settings: {
-      tailwindcss: {
-        // These are the default values but feel free to customize
-        callees: ["classnames", "clsx", "ctl"],
-        config: "tailwind.config.js", // returned from `loadConfig()` utility if not provided
-        cssFiles: [
-          "**/*.css",
-          "!**/node_modules",
-          "!**/.*",
-          "!**/dist",
-          "!**/build",
-        ],
-        cssFilesRefreshRate: 5_000,
-        removeDuplicates: true,
-        skipClassAttribute: false,
-        whitelist: [],
-        tags: [], // can be set to e.g. ['tw'] for use in tw`bg-blue`
-        classRegex: "^class(Name)?$", // can be modified to support custom attributes. E.g. "^tw$" for `twin.macro`
-      },
-    },
-  },
+  
+  
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
   {
