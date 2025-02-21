251fc4f286b147e318bd3dbe19daeb0610deeb56
laniakita <101172492+laniakita@users.noreply.github.com>
2025-02-21 12:57:58 -1000

    feat: disallow robots on dev subdomain + disable indexing common wp routes
    Updated robots.ts to gen dev and production robots.txt. Also, added more
    routes to disallow indexing on, such as common WP routes and potential
    auth routes.
    
    chore: bump contentlayer deps
    
    Updated package.json + bun.lock to use the latest contentlayer2 packages.

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -2,11 +2,33 @@ import type { MetadataRoute } from 'next';
 import { APP_URL } from '@/lib/constants';
 
 export default function robots(): MetadataRoute.Robots {
+  if (APP_URL.includes('https://dev.')) {
+    return {
+      rules: {
+        userAgent: '*',
+        disallow: ['/']
+      }, 
+      sitemap: `${APP_URL}/sitemap.xml`,
+    };
+  };
+
   return {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
+      disallow: [
+        '/private/',
+        '/user/',
+        '/users/',
+        '/auth/',
+        '/login*?*',
+        '/signup*?*',
+        '/opengraph/',
+        // WP crawlers
+        '/*/cgi-bin/',
+        '/*/wp-admin/',
+        '/*/xmlrpc.php'
+      ],
     },
     sitemap: `${APP_URL}/sitemap.xml`,
   };
