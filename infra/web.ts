ab558c2eb241f0f6e60fbd19dbd34c324a378965
laniakita <101172492+laniakita@users.noreply.github.com>
2025-02-22 11:56:07 -1000

    refactor: dev action

--- infra/web.ts
+++ infra/web.ts
@@ -1,6 +1,6 @@
 export const web = new sst.aws.Nextjs("Web", {
   path: "apps/web",
-  buildCommand: "turbo build:open-next",
+  buildCommand: "bun turbo build:open-next;",
   server: {
     runtime: "nodejs22.x"
   },
