4edb441868453e42061fd873137648228fad562e
laniakita <101172492+laniakita@users.noreply.github.com>
2025-03-03 15:41:53 -1000

    refactor: test if sst deploy command hangs in CI

--- infra/web.ts
+++ infra/web.ts
@@ -1,6 +1,6 @@
 export const web = new sst.aws.Nextjs("Web", {
   path: "apps/web",
-  buildCommand: "exit 0;",
+  buildCommand: "turbo run build:open-next",
   server: {
     runtime: "nodejs22.x"
   },
